9e21d36f8d29a290cc8f1c694ce73a8d4b6c74a7
Kweayon Clark <47009536+kwe92@users.noreply.github.com>
2022-07-06 18:05:14 -0400

    Add files via upload

---
+++ 1-Variables.dart
@@ -0,0 +1,53 @@
+/*
+
+The Dart language supports the following types−
+
+Numbers
+Strings
+Booleans
+Lists
+Maps
+Numbers
+
+Numbers in Dart are used to represent numeric literals. The Number Dart come in two flavours:
+
+Integer − Integer values represent non-fractional values, i.e., numeric values without a decimal point.
+          For example, the value "10" is an integer. Integer literals are represented using the int keyword.
+
+Double − Dart also supports fractional numeric values i.e. values with decimal points.
+         The Double data type in Dart represents a 64-bit (double-precision) floating-point number.
+         For example, the value "10.10".
+         The keyword double is used to represent floating point literals.
+
+Strings
+
+Strings represent a sequence of characters. For instance, if you were to store some data like name
+, address etc. the string data type should be used. A Dart string is a sequence of UTF-16 code units. 
+Runes are used to represent a sequence of UTF-32 code units.
+
+The keyword String is used to represent string literals.
+String values are embedded in either single or double quotes.
+
+Boolean
+
+The Boolean data type represents Boolean values true and false.
+Dart uses the bool keyword to represent a Boolean value.
+
+
+*/
+
+void main() {
+  String fairy = "Kira";
+
+  int num_cat = 1;
+
+  double pie = 3.14;
+
+  bool goddess = true;
+
+  // $ is a single variable format literal.
+  // Use ${} when more than one variable is being specified.
+
+  print(
+      "$fairy is a fairy goddess: $goddess with $num_cat cat who wants to know what is special about pie: $pie");
+}
