629af076ce0437c42a0a07ea3881435a3f151f36
Kweayon  Clark <Kweayon@gmail.com>
2022-08-27 13:11:26 -0500

    Update
    Added for loops to lesson 10 and more comments on lesson 10

--- 10-FunctionsGlobalMutableState.dart
+++ 10-FunctionsGlobalMutableState.dart
@@ -1,19 +1,39 @@
 // Globally Mutable State
 //    - A variable declared globally that is mutable
-var globalVariableCounter = 0;
+//    - Global mutal states are typically a really bad idea and should rarely be used
 
+var global_Mutable_Variable_Counter = 0;
+
+const global_Immutable_Variable = 0;
+
+/// The entry point to any program
 void main() {
+  // Non Pure Functions
+  //    - A function that modifies a variable outside of its scope
+  //      this is also called a side effect
   void increment() {
-    globalVariableCounter++;
+    global_Mutable_Variable_Counter++;
     print('I am a function that modifies the value of globalVariableCounter' +
         '\n' +
         'Which makes me a NON-PURE Function!' +
         '\n' +
-        'globalVariableCounter value: $globalVariableCounter');
+        'globalVariableCounter value: $global_Mutable_Variable_Counter');
   }
 
-  increment();
-  increment();
-  increment();
-  increment();
+  // Pure Function
+
+  // This function uses a global variable but does not modify it
+  // A pure function produces the same result when called with the same argument
+  num increment2([num i = 1]) {
+    num result = 0;
+    result += global_Immutable_Variable + i;
+    return result;
+  }
+
+  for (var i = 0; i < 3; i++) increment();
+  print(
+      'Result from calling increment2 a pure function: ${increment2()} | global_Immutable_Variable value: $global_Immutable_Variable');
+  for (var i = 0; i < 2; i++)
+    print(
+        'result of increment2(3): ${increment2(3)} | global_Immutable_Variable value: $global_Immutable_Variable');
 }
--- FirstClassObjects.dart
+++ FirstClassObjects.dart
@@ -1,14 +1,19 @@
+// typedef variants
+// typedef FavPhil = String Function(String); // option 1
+typedef String FavPhil(String name); // option 2
+List s = [];
 void main() {
   // TODO: Add anonymous function comments
   // TODO: Add First Class Object Comments
-  // TODO:
+  // TODO: Add comments for typedef and what it does
   var favPhilosopher =
       (philosopher) => '$philosopher, was a great philosopher.';
 
   print(favPhilosopher('Marcus Aurelius'));
 
   var firstClassObjectFunction =
-      (String Function(String) favPhil, philName) => {print(favPhil(philName))};
+      (FavPhil favPhil, philName) => {print(favPhil(philName))};
+  // (String Function(String) favPhil, philName) => {print(favPhil(philName))};
 
   firstClassObjectFunction(favPhilosopher, 'Aristotle');
 }
