4c809fe0db7130169687315b215322234442e036
Kweayon  Clark <Kweayon@gmail.com>
2022-08-29 17:22:55 -0500

    Update

--- Exercise_firstWhereFunction.dart
+++ Exercise_firstWhereFunction.dart
@@ -6,6 +6,24 @@
 //        if no element matches the search condition then return the value of 'orElse' function
 // Use Generics to work with values of any type
 
+import 'dart:io';
 import 'dart:math';
 
-void main() {}
+void main() {
+  List<int> intList = randIntList(10, 25);
+
+  print(intList);
+
+  print(firstWhere<int, int>(intList, (x) => x > 20, orElse: (() => -1)));
+}
+
+List<int> randIntList(var n, var r) =>
+    <int>[for (var i = 0; i < n; i++) Random().nextInt(r)];
+
+T firstWhere<T, R>(List<T> collection, bool Function(T) f,
+    {required T Function() orElse}) {
+  for (var x in collection) {
+    if (f(x)) return x;
+  }
+  return orElse();
+}
