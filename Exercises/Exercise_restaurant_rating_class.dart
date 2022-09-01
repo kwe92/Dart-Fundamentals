ab561b52e1c1471d650a5e69ee53d72a6803645f
Kweayon  Clark <Kweayon@gmail.com>
2022-09-01 17:32:14 -0500

    Update

---
+++ Exercises/Exercise_restaurant_rating_class.dart
@@ -0,0 +1,9 @@
+// Exercise: Restaurant rating with classes
+
+// Create a Class Restaurant:
+//    - 3 required kwargs name, cuisine, ratings  as String, String, List<double>
+
+
+// Add some functionality to this class so that it can be used to:
+// Get the total number of ratings
+// Get the average rating
