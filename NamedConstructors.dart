a50e0b3d6d5f7f53efda6f7e81ae172356b8f5de
Kweayon  Clark <Kweayon@gmail.com>
2022-09-01 11:33:55 -0500

    Update

--- NamedConstructors.dart
+++ NamedConstructors.dart
@@ -11,17 +11,21 @@ void main() {
   // Some Special Complex Numbers;
   // 0 + i * 0
   final Complex zero = Complex(0, 0);
+  final Complex zero2 = Complex.zero();
 
   // 1 + i * 0
   final Complex identity = Complex(1, 0);
+  final Complex identity2 = Complex.identity();
 
   // a + i * 0
   const double a = 3;
   final Complex real = Complex(a, 0);
+  final Complex real2 = Complex.real(a);
 
   // 0 + i * b
   const double b = 4;
   final Complex imaginary = Complex(0, b);
+  final Complex imaginary2 = Complex.imaginary(b);
 }
 
 // To initalize a const constructor the member variables must be:
