9b7e1207218b3c3159417b21af9f202cca0e1988
Kweayon  Clark <Kweayon@gmail.com>
2022-09-01 12:01:58 -0500

    Update
    Added code to exercise

--- Exercises/Exercise_NamedConstructors.dart
+++ Exercises/Exercise_NamedConstructors.dart
@@ -8,3 +8,17 @@
 // Use the named constructor to cinstruct a Temperature class
 // farehenheit to celsius formula:
 //    - (farenheit - 32) / 1.8
+
+void main() {
+  const TemperatureC tempc1 = TemperatureC(32);
+  final TemperatureC tempc2 = TemperatureC.fromFarenheit(90);
+}
+
+class TemperatureC {
+  const TemperatureC(this.celsius);
+  TemperatureC.fromFarenheit(double farenheit)
+      : celsius = convertFarenheit(farenheit);
+  final double celsius;
+}
+
+double convertFarenheit(double farenheit) => (farenheit - 32) / 1.8;
