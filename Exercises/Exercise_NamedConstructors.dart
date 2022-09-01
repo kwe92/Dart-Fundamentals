d1a8de67f9c107b57126abbecdf81f51711af7d4
Kweayon  Clark <Kweayon@gmail.com>
2022-09-01 11:53:10 -0500

    Update
    Added Exercise: Named Constructors

---
+++ Exercises/Exercise_NamedConstructors.dart
@@ -0,0 +1,10 @@
+// Create a Celsius Temperature Class
+// that can be created using celsius or farenheit
+// With two named constructors:
+//    - celsius and farenheit respectively
+// Member variables:
+//    - celsius(mutable)
+
+// Use the named constructor to cinstruct a Temperature class
+// farehenheit to celsius formula:
+//    - (farenheit - 32) / 1.8
