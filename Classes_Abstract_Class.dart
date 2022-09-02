9b91db933f6a1439402a0a9fa4e1256416ad98e4
Kweayon  Clark <Kweayon@gmail.com>
2022-09-02 16:49:43 -0500

    Update

--- Classes_Abstract_Class.dart
+++ Classes_Abstract_Class.dart
@@ -1,4 +1,3 @@
-import 'dart:html';
 import 'dart:math';
 
 abstract class Shape {
@@ -28,17 +27,36 @@ class Triangle extends Shape {
   final double c;
 
   @override
-  double get area => 1;
+  double get area => round(_getArea(a, b, c), 4);
+
+  double get height => round(_getArea(a, b, c) / (0.5 * b), 4);
+
+  double get base => b;
+
+  // Area of a triangle using Heron's Formula and a private method
+  double _getArea(var a, var b, var c) {
+    var s = (a + b + c) / 2;
+    var result = sqrt(s * ((s - a) * (s - b) * (s - c)));
+    return result;
+  }
 }
 
 void main() {
   final Shape c1 = Circle(6);
   final Shape s1 = Square(4);
+  final Triangle t1 = Triangle(9, 10, 11);
+
+  final List<Shape> listOfShapes = [c1, s1, t1];
 
   print('Area of c1: ${round(c1.area, 2)}');
   print('Area of s1: ${s1.area}');
+  print('Area of t1: ${t1.area}');
+  print('Height of t1: ${t1.height}');
+  print('Base of t1: ${t1.base}');
+
+  listOfShapes.forEach(printArea);
 }
 
-void printArea(Shape shape) => print(shape.area);
+void printArea(Shape shape) => print(round(shape.area, 2));
 
 double round(num n, [var p = 0]) => double.parse(n.toStringAsFixed(p));
