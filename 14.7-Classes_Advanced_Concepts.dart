393e8bf315c50dda008aa83441c7a5ec444374ca
Kweayon  Clark <Kweayon@gmail.com>
2022-09-03 10:05:24 -0500

    Update
    Added named constructors and tested them

--- 14.7-Classes_Advanced_Concepts.dart
+++ 14.7-Classes_Advanced_Concepts.dart
@@ -7,6 +7,7 @@ class SalaryException implements Exception {
 }
 
 abstract class BaseEmployee {
+  // default constructor
   BaseEmployee(this.fname, this.lname, this._salary) {
     if (_salary < 90000) {
       throw SalaryException(
@@ -14,34 +15,39 @@ abstract class BaseEmployee {
           .message;
     }
   }
+
+  // Named constuctor - (alt constructor)
+  BaseEmployee.fromString(String employee)
+      : fname = employee.split('-')[0],
+        lname = employee.split('-')[1],
+        _salary = double.parse(employee.split('-')[2]);
+
   final String fname;
   final String lname;
+  // Private Variable mutable variable _salary
   double _salary;
 
+  // Computed getter variables
   double get salary => _salary;
   String get fullname => '$fname $lname';
   String get email => '$fname.$lname@AyonInnovations.org';
 
-  // TODO: Add a named constructor to instantiate BaseEmployee from a string
-  void info();
+  String info() => 'Name: $fullname'
+      '\n'
+      'Salary: $salary'
+      '\n'
+      'E-mail: $email';
 }
 
 class Employee extends BaseEmployee {
   Employee(fname, lname, salary) : super(fname, lname, salary);
-  // Employee.fromString(s) : super.fromString(s); Initalizer list for alt named constructor
-
-  String info() {
-    return 'Name: $fullname'
-        '\n'
-        'Salary: $salary'
-        '\n'
-        'E-mail: $email';
-  }
+  Employee.fromString(s) : super.fromString(s);
 }
 
-class Programmer extends Employee {
+class Programmer extends BaseEmployee {
   Programmer(fname, lname, salary, {required this.progLang})
       : super(fname, lname, salary);
+  Programmer.fromString(s, this.progLang) : super.fromString(s);
 
   final progLang;
 
@@ -54,10 +60,15 @@ void main() {
 
   final Employee emp2 = Employee('Mugen', 'Infinity', 120000.00);
 
+  final Employee emp3 = Employee.fromString('Edward-Elric-500000.00');
+
   final Programmer prog1 =
       Programmer('Takagi', 'Fujimaru', 220000.00, progLang: 'C++');
 
-  // final Programmer prog2 =
+  final Programmer prog2 =
+      Programmer.fromString('Alphonse-Elric-320000.00', 'C++');
+
+  // final Programmer prog3 =
   // Programmer('Nine', 'Unknwon', 50000.00, progLang: 'Haskell');
 
   print('print 0: ${emp1.info()}');
@@ -69,6 +80,10 @@ void main() {
   print('print 3: ${emp2.fullname}');
   lineSpace();
   print('print 4: ${prog1.info()}');
+  lineSpace();
+  print('print 5: ${emp3.info()}');
+  lineSpace();
+  print('print 6: ${prog2.info()}');
 }
 
 void lineSpace() =>
