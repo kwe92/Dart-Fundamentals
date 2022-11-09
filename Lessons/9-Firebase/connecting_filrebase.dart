31eaf7c0d4ebdb9e943eb8cad426d2c5e5307f29
Kweayon  Clark <Kweayon@gmail.com>
2022-11-09 13:26:34 -0600

    Firebase Example
    Added TODO comments

---
+++ Lessons/9-Firebase/connecting_filrebase.dart
@@ -0,0 +1,5 @@
+// TODO: Create a console app that interacts with Firebase
+// TODO: Add Dependencies for firebase and firestore to pubspec.yaml
+// TODO: Create a firestore database for this project
+// TODO: Upload data to firestore
+// TODO: Stream live data from firestore
