3c1086a65949c9128d857bf1b0ceba456651cf9c
Kwe <Kweayon@gmail.com>
2023-04-06 12:40:08 -0500

    Parsing Strings

---
+++ Lessons/parsing/parsing_int_from_string.dart
@@ -0,0 +1,20 @@
+// TODO: Parse an integer from Payments Terms and return todays date plus that integer as days
+
+void main() {
+  const paymentTerm = 'Net 7 Days';
+  final now = DateTime.now();
+  print('Created Date: $now');
+  print('Payment Due Date: ${_paymentDue(paymentTerm)}');
+}
+
+DateTime _paymentDue(String paymentTerm) {
+  final date = DateTime.now();
+  final newDate =
+      new DateTime(date.year, date.month, date.day + parseInt(paymentTerm));
+
+  return newDate;
+}
+
+int parseInt(String s) => int.parse(s.replaceAll(new RegExp(r'[^0-9]'), ''));
+
+// a.replaceAll(new RegExp(r'[^0-9]'),'');
