85c991664f3015aa6b4af168ee5d8067f5a74be5
Kwe <Kweayon@gmail.com>
2023-06-09 11:57:00 -0500

    Records lesson 0

---
+++ Lessons/12-Records/0-records.dart
@@ -0,0 +1,15 @@
+// Records
+//    - Must use Dart SDK ^3.0 (Require language version at least 3.0.)
+//    - Records are:
+//        - An anonymous, immutable and aggregate type
+//        - Fixed size
+//        - FROZEN at compile time
+//        - heterogeneous (elements can be of multiple types)
+//        - typed
+//        - Bundle multiple objects into a single object
+//        - Record arguments can be both named and unnamed
+//        - Records are `real values` and can be used:
+//                - As variables
+//                - As nested values
+//                - Passed to and from function
+//                - Stored in data structures (lists, maps, and sets)
