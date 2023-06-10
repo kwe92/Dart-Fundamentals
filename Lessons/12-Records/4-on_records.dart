0d7d58ed2e7e8583fcab525cca3abf2036e0f211
Kwe <Kweayon@gmail.com>
2023-06-10 00:42:57 -0500

    Working with records

---
+++ Lessons/12-Records/4-on_records.dart
@@ -0,0 +1,53 @@
+typedef BookRecord = ({
+  String author,
+  String title,
+  int op,
+  String about,
+});
+
+BookRecord parseBook(Map<String, dynamic> json) => (
+      author: json["author"],
+      title: json["title"],
+      op: json["op"],
+      about: json["about"],
+    );
+
+class BookModel {
+  final String author;
+  final String title;
+  final int op;
+  final String about;
+
+  const BookModel({
+    required this.author,
+    required this.title,
+    required this.op,
+    required this.about,
+  });
+
+  factory BookModel.fromJSON(Map<String, dynamic> json) => BookModel(
+        author: json['author'],
+        title: json['title'],
+        op: json['op'],
+        about: json['about'],
+      );
+
+  @override
+  String toString() => 'BookModel(author: $author,title: $title,op: $op,about: $about,)';
+}
+
+const Map<String, dynamic> bookJSON = {
+  "author": "Shunryū Suzuki",
+  "title": "Zen Mind, Beginner's Mind",
+  "op": 1970,
+  "about":
+      "Zen Mind, Beginner's Mind is a book of teachings by Shunryu Suzuki, a compilation of talks given to his satellite Zen center in Los Altos, California.",
+};
+
+void main() {
+  print("bookJSON: ");
+  print(parseBook(bookJSON));
+
+  print("BookModel: ");
+  print(BookModel.fromJSON(bookJSON));
+}
