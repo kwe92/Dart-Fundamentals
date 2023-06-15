f0218e69de8cd6cfa7a63081a2d77940fd729eb9
Kwe <Kweayon@gmail.com>
2023-06-15 12:27:45 -0500

    interface modifier

---
+++ Lessons/6-OOP/16.3-Interface-Class-Modifier.dart
@@ -0,0 +1,31 @@
+// interface class modifier
+
+//   - interface classes can not be extended (inherited) outside of the module (library)
+//   - interface classes can be implemented
+
+interface class TodoInterface {
+  final String id;
+  final String todo;
+  final bool isComplete;
+  const TodoInterface({
+    required this.id,
+    required this.todo,
+    required this.isComplete,
+  });
+
+  void checkTodo() {}
+}
+
+class Todo extends TodoInterface {
+  const Todo({required super.id, required super.todo, required super.isComplete});
+}
+
+void main() {
+  const todo = Todo(
+    id: '',
+    todo: '',
+    isComplete: false,
+  );
+
+  print(todo.isComplete);
+}
