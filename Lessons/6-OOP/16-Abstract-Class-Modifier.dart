79e2bd4369a964da4894834d6c8632984ac8cf7f
Kwe <Kweayon@gmail.com>
2023-06-15 12:54:25 -0500

    changed giveRaise to be abstract

--- Lessons/6-OOP/16-Abstract-Class-Modifier.dart
+++ Lessons/6-OOP/16-Abstract-Class-Modifier.dart
@@ -3,7 +3,7 @@
 // Class Modifiers
 
 //   - Prefix class declaration
-//   - Controls how and where a class can be used
+//   - Adds additional features and restrictions to class declarations
 
 // abstract class modifier
 
@@ -11,29 +11,44 @@
 //  - Sub-types do not require full concrete implementation of entire interface
 //  - You don't need to implement the function signatures defined by the super
 
+// abstract methods
+
+//   - replace method body with semi-colon indicates to the compiler that the method is abstract
+//   - Only abstract classes can have abstract methods
+
 abstract class Employee {
   final String fname;
   final String lname;
-  final double salary;
+  double salary;
   String get email => "${fname.substring(0, 1).toLowerCase()}${lname.toLowerCase()}@ayonInnovations.net";
 
-  const Employee({
+  Employee({
     required this.fname,
     required this.lname,
     required this.salary,
   });
 
-  void giveRaise(double x) {}
+  // an abstract method that must be implemented by the sup-type
+  void giveRaise(double x);
 }
 
 class SoftwareEngineer extends Employee {
   final List<String> progLangs;
-  const SoftwareEngineer({
+  SoftwareEngineer({
     required super.fname,
     required super.lname,
     required this.progLangs,
     required super.salary,
   });
+
+  @override
+  void giveRaise(double x) {
+    x <= 0.15
+        ? this.salary = this.salary + (this.salary * x)
+        : print(
+            "raise can not exceed 15%",
+          );
+  }
 }
 
 int main() {
@@ -53,6 +68,8 @@ int main() {
   );
 
   print(emp1.email);
+
+  print(emp1);
 
   return 0;
 }
