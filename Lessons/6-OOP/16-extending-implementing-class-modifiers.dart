5ecf384fc9261bd688cb45cdbc0df0cb574214d7
Kwe <Kweayon@gmail.com>
2023-06-16 11:50:33 -0500

    extending and implementing class modifiers

---
+++ Lessons/6-OOP/16-extending-implementing-class-modifiers.dart
@@ -0,0 +1,42 @@
+// TODO: Implement and extend class modifier examples
+import '16._-Final-Class-Modifier.dart';
+
+// Extending final classes
+class EmpClass extends FinalEmployee {
+  EmpClass({
+    required super.fname,
+    required super.lname,
+    required super.salary,
+  });
+}
+
+// Implementing final classes
+class EmpImplement implements FinalEmployee {
+  @override
+  // TODO: implement fname
+  String get fname => throw UnimplementedError();
+
+  @override
+  void giveRaise(double raisePct) {
+    // TODO: implement giveRaise
+  }
+
+  @override
+  // TODO: implement lname
+  String get lname => throw UnimplementedError();
+
+  @override
+  // TODO: implement salary
+  double get salary => throw UnimplementedError();
+}
+
+void main() {
+  // final classes can be instantiated (constructed)
+  final finalEmp0 = FinalEmployee(
+    fname: 'Gon',
+    lname: 'Freecss',
+    salary: 45000,
+  );
+
+  print(finalEmp0);
+}
