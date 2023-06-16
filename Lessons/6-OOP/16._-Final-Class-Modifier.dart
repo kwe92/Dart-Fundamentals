ea8e6e5282bb9d88c65098f8d435a1a510ac8e81
Kwe <Kweayon@gmail.com>
2023-06-16 10:59:57 -0500

    final class modifier

---
+++ Lessons/6-OOP/16._-Final-Class-Modifier.dart
@@ -0,0 +1,37 @@
+// final class modifier
+
+//   - final classes:
+
+//     - Prevents subtyping outside of its own library
+//     - Can not be extended (inherited)
+//     - Can not be implemented
+
+final class FinalEmployee {
+  final String fname;
+  final String lname;
+  late double _salary;
+  double get salary => _salary;
+  FinalEmployee({
+    required this.fname,
+    required this.lname,
+    required double salary,
+  }) {
+    this._salary = salary;
+  }
+
+  void giveRaise(double raisePct) =>
+      raisePct <= 0.16 ? this._salary = this._salary + (this._salary * raisePct) : print('raise percent must not exceed 15%');
+}
+
+// Can be extended within its own library
+base class ExtendedEmployee extends FinalEmployee {
+  ExtendedEmployee({
+    required super.fname,
+    required super.lname,
+    required super.salary,
+  });
+}
+
+int main() {
+  return 0;
+}
