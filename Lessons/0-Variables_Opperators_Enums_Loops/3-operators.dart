709247c97558e7963f7e49cf962f1e70d76a2816
Kwe <Kweayon@gmail.com>
2023-06-19 12:35:15 -0500

    Upadte

---
+++ Lessons/0-Variables_Opperators_Enums_Loops/3-operators.dart
@@ -0,0 +1,47 @@
+void main() {
+  int y = 5;
+
+  int num1 = 42;
+  int num2 = 12;
+
+  print(num1.runtimeType);
+  print(num2.runtimeType);
+
+  var sum_num1_num2 = num1 + num2; // adding variables
+
+  print('the sum of $num1 + $num2 is $sum_num1_num2.');
+
+  // Working with operators
+
+  // Logical AND
+
+  //   - && represents AND
+
+  print(sum_num1_num2 >= num1 && sum_num1_num2 == num1 + num2);
+
+  print(10 % 2); // returns the remander
+
+  // Logical OR
+
+  //   - || represents OR
+
+  print(sum_num1_num2 >= num1 || num1 < num2);
+
+  // is keyword
+
+  //   - returns turns true if the condition is met false otherwise
+
+  print(sum_num1_num2 is! bool); // is! == is not
+
+// Augmented operators
+
+  y += 5; // the same as y = y + 5;
+
+  y++; // the same as y = y + 1;
+
+  y--; // the same as y = y - 1;
+
+  y *= 3; // the same as y = y * 3;
+
+  print(y);
+}
