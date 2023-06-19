2557269a62e71136a82f46eafdde76b582de1489
Kwe <Kweayon@gmail.com>
2023-06-19 18:00:27 -0500

    New feature: if clause statements

---
+++ Lessons/0-Variables_Operators_Enums_Loops/4.1-if_case_statement.dart
@@ -0,0 +1,39 @@
+// if case statements
+
+//   - Dart if statements now support if clauses followed by a pattern
+//   - A way to match the destructure ]of an object to a single pattern
+
+class Point {
+  final double x;
+  final double y;
+  const Point({
+    required this.x,
+    required this.y,
+  });
+
+  @override
+  String toString() => "Point($x, $y)";
+}
+
+const List<double> pair = [4.0, 2.0];
+const List<double> triplet = [2.12, 43.3, 5.6];
+
+Point? getPoint(Iterable pair) {
+  if (pair case [double x, double y]) {
+    return Point(x: x, y: y);
+  }
+  return null;
+}
+
+void main() {
+  final Point? p1 = getPoint(pair);
+  final Point? p2 = getPoint(triplet);
+
+  print('Point 1: $p1');
+  print('Point 2: $p2');
+}
+
+// Summary
+
+// - With the introduction of patterns you can now use case clauses in if-statements
+// - If the pattern matches the value then the branch executes with any variables the pattern defines in scope
