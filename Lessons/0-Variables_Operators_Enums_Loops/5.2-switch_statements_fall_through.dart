a933c54136e25058a7991a62a34ee3c92a08152a
Kwe <Kweayon@gmail.com>
2023-06-20 10:25:45 -0500

    Switch statements - fall through

---
+++ Lessons/0-Variables_Operators_Enums_Loops/5.2-switch_statements_fall_through.dart
@@ -0,0 +1,32 @@
+// Switch statement - fall through
+
+//   - empty cases fall through to the next case
+//       - use break in an empty case's body to prevent fall through
+
+// Switch statement - non-sequential fallthrough
+//   - use the continue statement and a label
+
+void executePaid() => print('paid, closing and shipping.');
+void executeShip() => print('package has been shipped.');
+void executeNotify() => print('unpaid, notifying customer via email.');
+
+void main() {
+  final List<String> status = [
+    'PAID',
+    'SHIP',
+    'UNPAID',
+    'NOTIFY',
+  ];
+
+  switch (status[0]) {
+    case 'PAID':
+      executePaid();
+      continue shipItem;
+    shipItem:
+    case 'SHIP':
+      executeShip();
+    case 'UNPAID':
+    case 'NOTIFY':
+      executeNotify();
+  }
+}
