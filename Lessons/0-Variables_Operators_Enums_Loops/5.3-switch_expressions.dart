4f8d09192dd947e2ac9fec56d3b45374e7853fc5
Kwe <Kweayon@gmail.com>
2023-06-20 12:11:27 -0500

    switch statement expressions

--- Lessons/0-Variables_Operators_Enums_Loops/5.3-switch_expressions.dart
+++ Lessons/0-Variables_Operators_Enums_Loops/5.3-switch_expressions.dart
@@ -4,22 +4,38 @@ typedef VonNeumannModel = ({
   String memory,
   String storage,
 });
-final VonNeumannModel MacRecord = (
+final VonNeumannModel macRecord = (
   os: 'macOS Ventura 13.0.1',
   cpu: 'Apple M1',
   memory: '8gb',
   storage: 'Macintoch HD 250gb',
 );
-final VonNeumannModel LinuxRecord = (
+final VonNeumannModel linuxRecord = (
   os: 'Ubuntu',
   cpu: 'Intel Pentium Gold Processor 8505',
   memory: '16gb',
   storage: 'Seagate Exos X20 20tb',
 );
-final VonNeumannModel WindowsRecord = (
+final VonNeumannModel windowsRecord = (
   os: 'Windows 11',
   cpu: 'Intel Atom x6200FE',
   memory: '32gb',
   storage: 'Seagate BarraCuda 8TB HDD',
 );
-void main() {}
+
+void main() {
+  final List<String> os = [
+    'mac',
+    'windows',
+    'linux',
+  ];
+
+  final computer = switch (os[1]) {
+    'mac' => macRecord,
+    'windows' => windowsRecord,
+    'linux' => linuxRecord,
+    _ => 'not a known operating system',
+  };
+
+  print(computer);
+}
