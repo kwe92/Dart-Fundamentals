b7bba70d34f32579b43a2a96210c2a163420a7bb
Kwe <Kweayon@gmail.com>
2023-06-21 10:53:04 -0500

    Expanded lessi=on

--- Lessons/0-Variables_Operators_Enums_Loops/5.5-switch_statements_guard_clause.dart
+++ Lessons/0-Variables_Operators_Enums_Loops/5.5-switch_statements_guard_clause.dart
@@ -1,7 +1,7 @@
 // Switch Statement: Guard Clause
 
 //   - evaluates an arbitrary boolean expression after matching
-//   - add addition constraints
+//   - adds additional constraints
 //   - If a gaurd clause is false it will fallthrough to the next case statement
 
 class Point {
@@ -16,15 +16,24 @@ class Point {
 
   @override
   String toString() => 'Point($x, $y)';
+
+  @override
+  bool operator ==(Object other) => identical(this, other) ? true : other is Point && other.x == x && other.y == y;
+
+  @override
+  int get hashCode => x.hashCode ^ y.hashCode;
 }
 
+// function returning a switch expression
 Point getPoint(Pair pair) => switch (pair) {
       (double a, double b) when a >= b && b > 0 => Point(x: a, y: b),
-      _ => throw FormatException(),
+      _ => throw PointException('x must be greater than y and y must be greater than 0.'),
     };
 
+// type signature
 typedef Pair = (double a, double b);
 
+// custom exception
 class PointException extends Error {
   final String? message;
   PointException([this.message]);
@@ -34,7 +43,19 @@ class PointException extends Error {
 }
 
 void main() {
-  const Pair arr1 = (4.0, 2.0);
-  final Point p1 = getPoint(arr1);
-  print(p1);
+  try {
+    const Pair pair0 = (3, -7);
+    final Point p0 = getPoint(pair0);
+    print(p0);
+  } catch (errorMsg, stackTrace) {
+    print('\n$errorMsg\n');
+
+    stackTrace.toString().split('#').forEach((stackTrackMessage) {
+      print('$stackTrackMessage\n');
+    });
+  } finally {
+    const Pair pair1 = (4.0, 2.0);
+    final Point p1 = getPoint(pair1);
+    print('Point1: $p1');
+  }
 }
