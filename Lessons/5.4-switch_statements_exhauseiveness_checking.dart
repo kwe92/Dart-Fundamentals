b16473c8d386f4f231c163b5b25bd284407d931e
Kwe <Kweayon@gmail.com>
2023-06-21 09:34:48 -0500

    expanded lesson and updated comments

--- Lessons/5.4-switch_statements_exhauseiveness_checking.dart
+++ Lessons/5.4-switch_statements_exhauseiveness_checking.dart
@@ -1,5 +1,17 @@
 import 'dart:math';
 
+// Exhaustiveness: Switch Statements
+
+//   - All possible outcomes must be exhausted
+//   - compiletime error if a value does not match a case within a switch statement
+
+// Enumerated and sealed types
+
+//   - enumerated and sealed types do not require a default case
+//   - all possible values are known and fully enumerable
+//   - implies that a switch on any type is exhaustive
+//       - does not require a default case or _ (wild card pattern)
+
 sealed class Shape {
   const Shape();
 }
@@ -31,9 +43,19 @@ double getArea(Shape shape) => switch (shape) {
 
 void main() {
   const square = Square(length: 19);
-  const circle = Circle(radius: 1);
-  final circleArea = getArea(circle);
+  const circle = Circle(radius: 3);
 
+//  This code will fail as not all possible outcomes are covered
+
+//   final bool? boolVal = true;
+//   switch (boolVal) {
+//     case true:
+//       print('yes');
+//     case false:
+//       print('no');
+//   }
+
+  final circleArea = getArea(circle);
   final squareArea = getArea(square);
 
   print('$square has an area of $squareArea');
