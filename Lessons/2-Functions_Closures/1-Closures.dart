01ccd1c9153b2de7f15134c181270d858594dde4
Kwe <Kweayon@gmail.com>
2023-06-24 10:05:46 -0500

    refactor: completly changed lessand and comments

--- Lessons/2-Functions_Closures/1-Closures.dart
+++ Lessons/2-Functions_Closures/1-Closures.dart
@@ -1,17 +1,19 @@
 // Closure:
 
 //   - anonymous function
-//   - they have access to variables inside their lexical scope
+//   - have access to variables inside their lexical scope
 //   - even when a function is used outside of its original scope
 
 typedef IntFunc = int Function(int);
 
+// returns an anonymous function
+// can be used anywhere an anonymous function can be used
 IntFunc makeAdder(int addBy) => (int x) => x + addBy;
 
 /// The entry point of any program
 void main() {
-  final adder3 = makeAdder(3);
-  final adder7 = makeAdder(7);
+  final IntFunc adder3 = makeAdder(3);
+  final IntFunc adder7 = makeAdder(7);
 
   print('adder3 value: ${adder3(39)}');
   print('adder7 value: ${adder7(39)}');
