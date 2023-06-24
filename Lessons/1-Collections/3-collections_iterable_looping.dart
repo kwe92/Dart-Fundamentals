a862ec824a6ca545f226d9e5ac449b4eaa0c6bfa
Kwe <Kweayon@gmail.com>
2023-06-23 22:47:17 -0500

    new lesson - iterable looping

---
+++ Lessons/1-Collections/3-collections_iterable_looping.dart
@@ -0,0 +1,26 @@
+void main() {
+  List<String> heroNames = [
+    'Gon',
+    'Deku',
+    'Gara',
+    'Shikamaru',
+  ];
+
+  // Iterable iteration
+
+  // forEach
+  heroNames.forEach(
+    (element) => print(
+      element.toLowerCase(),
+    ),
+  );
+
+  // for-in
+  for (var hero in heroNames) {
+    print(hero);
+  }
+  // for loop
+  for (var i = 0; i < heroNames.length; i++) {
+    print(heroNames[i].toUpperCase());
+  }
+}
