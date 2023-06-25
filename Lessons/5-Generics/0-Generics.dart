a7b9cadeb3d0579fcb4a627c5a782e25afd051d0
Kwe <Kweayon@gmail.com>
2023-06-25 10:35:33 -0500

    Updated comments

--- Lessons/5-Generics/0-Generics.dart
+++ Lessons/5-Generics/0-Generics.dart
@@ -6,8 +6,7 @@ void main() {
   const myList = [1, 2, 3];
 
   // Transformations Using .map method
-  final List<double> transformedList =
-      myList.map((element) => element * 2.toDouble()).toList();
+  final List<double> transformedList = myList.map((element) => element * 2.toDouble()).toList();
 
   print('Transformed list using .map method: $transformedList');
 
@@ -20,8 +19,7 @@ void main() {
 
   var l1 = transform<int, int>([1, 3, 5, 7, 9], (x) => x * 2);
   var l2 = transform<double, double>([1, 3, 5, 7, 9], (x) => x / 2);
-  var l3 = transform<double, int>(
-      [1.3, 3.6, 5.456, 7.1243, 9.976], (x) => x.round());
+  var l3 = transform<double, int>([1.3, 3.6, 5.456, 7.1243, 9.976], (x) => x.round());
 
   print('l1 run time type: ${l1.runtimeType} | value: ${l1}');
   print('l2 run time type: ${l2.runtimeType} | value: ${l2}');
@@ -33,12 +31,10 @@ void main() {
 //    - Dynamically creates a new list using collection-for
 //    - Returns a new List
 
-List<R> transform<T, R>(List<T> items, R Function(T) f) =>
-    <R>[for (var x in items) f(x)];
+List<R> transform<T, R>(List<T> items, R Function(T) f) => <R>[for (var x in items) f(x)];
 
 // doubleitems
 //    - Doubles the items in a list and returns a list
 //    - returns a new transformed list
 
-List<int> doubleItems(List<int> list) =>
-    <int>[for (var element in list) element * 2];
+List<int> doubleItems(List<int> list) => <int>[for (var element in list) element * 2];
