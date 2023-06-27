5fc42fb0e4ec1ece6b0cc5bfbca71c114260d186
Kwe <Kweayon@gmail.com>
2023-06-27 02:18:06 -0500

    updated lesson

--- Lessons/6-OOP/_-getters_setters.dart
+++ Lessons/6-OOP/_-getters_setters.dart
@@ -1,11 +1,41 @@
+// getter
 
-  // getter
+//   - special method providing read access to:
+//       - member variables and computed variables
+//   - generated implicitly for member vairables
+//   - a derivation process generating a derivative (computed variable)
+//   - returns a primative or a combination of primatives and member variables
+//   - final by default
 
-  //   - a special method that provides read access to member variables and computed variables
-  //   - generated implicitly for member vairables
-  //   - can also be computed as a derivation process that generates a derivative
-  //   - can be a primative or a combination of primatives and member variables
-  //   - final by default
+// setter
 
-  // setter
-  //  - 
+//  - special method that provides write access to mutable member variables
+
+// Nullable mutable Class
+//   - constructor can be ommited
+
+class Employee {
+  String? fname, lname;
+  double? salary;
+  // computed variable
+  String? get email => '$fname.$lname@ayonInovations.net';
+
+  // print string representation of Object
+  @override
+  String toString() => 'Employee(fname: $fname , lname: $lname, salary: $salary, email: $email)';
+}
+
+void main() {
+  // initally all member variables are null
+  Employee emp1 = Employee();
+
+  print('emp1: $emp1');
+
+  // using the setter method for member variables
+  emp1
+    ..fname = 'Kweayon'
+    ..lname = 'Harshen-wuta'
+    ..salary = 175000;
+
+  print('emp1 after setting variables: $emp1');
+}
