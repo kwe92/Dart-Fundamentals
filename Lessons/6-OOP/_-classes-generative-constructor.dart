44afcbd62ab0f5fee9311b12f5986968d8426cd0
Kwe <Kweayon@gmail.com>
2023-06-27 15:47:35 -0500

    inital commit of new lesson

---
+++ Lessons/6-OOP/_-classes-generative-constructor.dart
@@ -0,0 +1,11 @@
+// Declaring a Class Constructor
+
+//   - Create a method with an identical name as the class
+//   - which is then flaged by the compiler as a class constructor
+
+// Generative Constructor
+
+//   - the most common form of a constructor in all programming languages
+//   - creates a new instance of an Object
+
+class Employee {}
