a8287bfaeb068528f8cb1bd2eba654caca41a83c
Kwe <Kweayon@gmail.com>
2023-06-29 09:13:53 -0500

    Update

--- Lessons/6-OOP/3-classes_named_constructors.dart
+++ Lessons/6-OOP/3-classes_named_constructors.dart
@@ -21,16 +21,15 @@ class Vehicle {
         motorVehicle = motorVehicle ?? false,
         electricVehicle = electricVehicle ?? false,
         color = color;
-// TODO: change to named parameters
-  Vehicle.sedan(String color, [bool? motorVehicle, bool? electricVehicle])
+
+  Vehicle.sedan({required String color, bool? motorVehicle, bool? electricVehicle})
       : wheels = 4,
         passangers = 5,
         motorVehicle = motorVehicle ?? true,
         electricVehicle = electricVehicle ?? false,
         color = color;
-// TODO: change to named parameters
 
-  Vehicle.truck(String color, [bool? motorVehicle, bool? electricVehicle])
+  Vehicle.truck({required String color, bool? motorVehicle, bool? electricVehicle})
       : wheels = 18,
         passangers = 3,
         motorVehicle = motorVehicle ?? true,
@@ -40,10 +39,27 @@ class Vehicle {
 
 void main() {
   final Vehicle bicycle = Vehicle.bicycle(color: 'Orange');
+
   final Vehicle motorCycle = Vehicle.bicycle(
     color: 'Black',
     motorVehicle: true,
     electricVehicle: false,
   );
-  // final Vehicle
+
+  final Vehicle tesla = Vehicle.sedan(
+    color: 'Royal Blue',
+    motorVehicle: false,
+    electricVehicle: true,
+  );
+
+  final Vehicle hondaAccordHybrid = Vehicle.sedan(
+    color: 'Royal Blue',
+    electricVehicle: true,
+  );
+
+  final Vehicle teslaSemiTruck = Vehicle.truck(
+    color: 'Silver',
+    motorVehicle: false,
+    electricVehicle: true,
+  );
 }
