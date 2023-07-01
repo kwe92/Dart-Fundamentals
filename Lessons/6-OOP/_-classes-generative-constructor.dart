53ec263e0748c27ad2ae0122152307fb9aea3b8a
Kwe <Kweayon@gmail.com>
2023-06-30 19:10:46 -0500

    Updated comments and expanded lesson

--- Lessons/6-OOP/_-classes-generative-constructor.dart
+++ Lessons/6-OOP/_-classes-generative-constructor.dart
@@ -1,15 +1,17 @@
 // Declaring a Class Constructor
 
-//   - Create a method with an identical name as the class
-//   - which is then flaged by the compiler as a class constructor
+//   - Implement a constructor function within a class body
+//   - which is a special method that has a name identical to the class name
+//   - class constructors are then flaged by the compiler as special methods
 
 // Generative Constructor
 
-//   - most common form of constructor in all programming languages
+//   - the most common form of a constructor in all OOP languages
 //   - creates a new instance of an Object
 
 class AnimeCharacter {
   String fname, lname, anime;
+  // Generative Constructor Function
   AnimeCharacter(this.fname, this.lname, this.anime) {
     this.fname = fname;
     this.lname = lname;
@@ -17,13 +19,21 @@ class AnimeCharacter {
   }
 }
 
-// Initalizing formal parameters
+// Generative Constructor Function Summary
 
-//   - member variables can be initalized before the constructor body
-//   - you can ommit the above constructor body for brevity and less code
+//   - the most verbose constructor
+//   - member variables are declared (assigned a datatype) above the constructor function
+//   - member variables are passed into the constructor function via this.memberVariable
+//   - member variables are then initalized in the constructor function body
+//   - this.memberVariable = memberVariable where both names are identical
+
+// Initalizing FormalParameters
+
+//   - member variables can be initalized before the constructor body (in the head of the constructor function)
+//   - the above constructor body can be ommited for brevity and less code
 //   - non-nullable variables must be initalized or assined a default value
 
-// non-nullable variables
+// Mutable(Stateful) Class with non-nullable variables
 
 class AnimeCharacter2 {
   String fname, lname, anime;
@@ -33,8 +43,9 @@ class AnimeCharacter2 {
 // nullable variables
 
 //   - constructor can be ommited
-//   - variables flagged as null get initalized as Null objects
-//   - if constructor is ommited there is a no arguement constructor generated
+//   - variables declared as null get initalized as Null objects implicitly
+//   - if the constructor is ommited there is a unnamed no arguement constructor generated
+//   - no arguement constructor means you must instantiate the class before initalizing variables
 
 class AnimeCharacter3 {
   String? fname, lname, anime;
@@ -58,6 +69,11 @@ void main() {
     ..fname = 'Shikamaru'
     ..lname = 'Nara'
     ..anime = 'Naruto';
+
+  final AnimeCharacter3 animeChar4 = AnimeCharacter3()
+    ..fname = 'Houtarou'
+    ..lname = 'Oreki'
+    ..anime = 'Hyouka';
 
   print('\nOne of my favorite animes: ${animeCharacter.anime}\n');
   print('\nAnother one of my favorite animes: ${animeCharacter2.anime}\n');
