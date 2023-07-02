2730080f6126d61a692181845894facc2ad14725
Kwe <Kweayon@gmail.com>
2023-07-02 08:24:10 -0500

    New Lesson: named constructors - Factory Constructor

---
+++ Lessons/6-OOP/_-classes_factory_constructors.dart
@@ -0,0 +1,54 @@
+// Factory Constructor
+
+//   - a constructor implementation that doesn't
+//     always create a new instance of its class
+//   - may return an instance from a cache
+//   - may return an instance of a subtype
+//   - initalizing a final variable
+//     using logic that can't be handled in an initalizer list
+
+class Logger {
+  final String name;
+
+  bool mute = false;
+
+  // private class variable
+  static final Map<String, Logger> _cache = <String, Logger>{};
+
+  // Initalizer list (Named Constructor) to create a Logger from JSON data
+  Logger.fromJSON(Map<String, String> json) : name = json['name'].toString();
+
+  factory Logger(String name) => _cache.putIfAbsent(
+        name,
+        () => Logger._internal(name),
+      );
+
+  // factory methods are considered a unnamed constructor
+  // so you must use a named constructor
+  Logger._internal(this.name);
+
+  void log(String msg) => !mute ? print(msg) : null;
+
+  // generative constructors can not redirect to factory constructors
+  // Logger._internal(String name) : this(name);
+}
+
+Future<Map<String, String>> useFetchLoggerApi() async {
+  await Future.delayed(
+    Duration(seconds: 1, milliseconds: 200),
+  );
+
+  return {'name': 'U.I.'};
+}
+
+Future<void> main() async {
+  Logger logger = Logger('U.I.');
+
+  logger.log('button clicked');
+
+  Logger loggerJson = Logger.fromJSON(
+    await useFetchLoggerApi(),
+  );
+
+  print('loggerJson name: ${loggerJson.name}');
+}
