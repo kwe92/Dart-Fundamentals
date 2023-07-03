6449450a128c5204d1eef87ff9409cfd11e197fc
Kwe <Kweayon@gmail.com>
2023-07-03 08:25:13 -0500

    updated comments and expanded lesson

--- Lessons/6-OOP/_-classes_factory_constructors.dart
+++ Lessons/6-OOP/_-classes_factory_constructors.dart
@@ -4,11 +4,11 @@
 
 //   - a constructor implementation that doesn't
 //     always create a new instance of its class
-//   - Can be either unnamed and named
+//   - Can be either unnamed or named
 //   - may return an instance from a cache
 //   - may return an instance of a subtype
-//   - initalizing a final variable
-//     using logic that can't be handled in an initalizer list
+//   - initalize final variables using logic
+//     that can't be handled in an initalizer list
 
 class Logger {
   final String name;
@@ -17,7 +17,7 @@ class Logger {
 
   // private class variable
 
-  static final Map<String, Logger> _cache = <String, Logger>{};
+  static final _cache = <String, Logger>{};
 
   // Named Factory Constructor
 
@@ -27,20 +27,32 @@ class Logger {
         json['name'].toString(),
       );
 
+  // Generative Factory Constructor
+
+  //   - in this example the generative factory constructor
+  //     adds a Logger to the class instance variable _cache
+  //     if the log does not already exist with the specified key
+  //   - Does not return a new instance of the class
+
   factory Logger(String name) => _cache.putIfAbsent(
         name,
         () => Logger._internal(name),
       );
 
-  // factory methods are considered a unnamed constructor
-  // so you must use a named constructor
+  // Unnamed Generative Constructor
+  //   - classes can only have one unnamed generative constructor
+  //   - thus a named constructor is required to
+  //     initalize instance variables and return the class
+  //     if the unnamed constructor does not do so
 
   Logger._internal(this.name);
 
   void log(String msg) => !mute ? print(msg) : null;
 
-  // generative constructors can not redirect to factory constructors
-  // Logger._internal(String name) : this(name);
+  // Generative Constructor Redirection:
+
+  //   - can not redirect to factory constructors
+  //   - Logger._internal(String name) : this(name);
 }
 
 Future<Map<String, String>> useFetchLoggerApi() async {
