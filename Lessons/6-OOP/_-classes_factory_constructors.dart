fa3f542576d5a5eca444934c55e2587f3592e646
Kwe <Kweayon@gmail.com>
2023-07-04 08:15:19 -0500

    refactor: added brevity to comments

--- Lessons/6-OOP/_-classes_factory_constructors.dart
+++ Lessons/6-OOP/_-classes_factory_constructors.dart
@@ -1,58 +1,55 @@
-// TODO: Review, if no changes do not commit
-
 // Factory Constructor
 
-//   - a constructor implementation that doesn't
-//     always create a new instance of its class
-//   - Can be either unnamed or named
-//   - may return an instance from a cache
-//   - may return an instance of a subtype
-//   - initalize final variables using logic
-//     that can't be handled in an initalizer list
+//   - can be either unnamed or named
+//   - doesn't always create new instances of its type
+//   - has access to the constructor body allowing
+//     logic that can't be handled in an initalizer list
+
+// Named Factory Constructor (Identifier Constructor)
+
+//   - named constructors MUST return an instance of their type
+//   - named Factory Constructors can have a function body
+//     allowing futher manipulation of data before their type is returned
+//   - Initalizer Lists can ONLY initalize variables
+//     and do not have access to a function body
+
+// Unnamed Factory Constructors
+
+//   - a generative constructor
+//   - does not have to return an instance of its type
+
+// Generative Constructor Redirection:
+
+//   - can not redirect to factory constructors
+//   - Logger._internal(String name) : this(name);
 
 class Logger {
   final String name;
 
   bool mute = false;
 
-  // private class variable
+  // static variables
+
+  //   - class variables
 
   static final _cache = <String, Logger>{};
-
-  // Named Factory Constructor
-
-  //   - named constructors MUST return an instance of their class
 
   factory Logger.fromJSON(Map<String, String> json) => Logger(
         json['name'].toString(),
       );
-
-  // Generative Factory Constructor
-
-  //   - in this example the generative factory constructor
-  //     adds a Logger to the class instance variable _cache
-  //     if the log does not already exist with the specified key
-  //   - Does not return a new instance of the class
 
   factory Logger(String name) => _cache.putIfAbsent(
         name,
         () => Logger._internal(name),
       );
 
-  // Unnamed Generative Constructor
-  //   - classes can only have one unnamed generative constructor
-  //   - thus a named constructor is required to
-  //     initalize instance variables and return the class
-  //     if the unnamed constructor does not do so
+// Named Generative Constructor
+
+//   - to initalize instance variables and create a type
 
   Logger._internal(this.name);
 
   void log(String msg) => !mute ? print(msg) : null;
-
-  // Generative Constructor Redirection:
-
-  //   - can not redirect to factory constructors
-  //   - Logger._internal(String name) : this(name);
 }
 
 Future<Map<String, String>> useFetchLoggerApi() async {
