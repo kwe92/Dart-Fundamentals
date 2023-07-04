b4ddb037d24b39bbc0084413a9870cb4fc48eccc
Kwe <Kweayon@gmail.com>
2023-07-04 10:58:26 -0500

    refactor: expanded lesson added brevity to comments

--- Lessons/6-OOP/_-classes_factory_constructors.dart
+++ Lessons/6-OOP/_-classes_factory_constructors.dart
@@ -1,56 +1,67 @@
 // Factory Constructor
 
-//   - can be either unnamed or named
-//   - doesn't always create new instances of its type
-//   - has access to the constructor body allowing
-//     logic that can't be handled in an initalizer list
+//   - can be either:
 
-// Named Factory Constructor (Identifier Constructor)
+//       -  a unnamed constructor (Generator Constructor)
+//       -  a named constructor (Identifier Constructor)
 
-//   - named constructors MUST return an instance of their type
-//   - named Factory Constructors can have a function body
-//     allowing futher manipulation of data before their type is returned
-//   - Initalizer Lists can ONLY initalize variables
-//     and do not have access to a function body
+// Factory Named Constructor (Identifier Constructor)
+
+//   - ALL named constructors MUST return an instance of their type
+//   - factory named constructors have access to the constructor body
+//     allowing logic that can't be handled in an initalizer list
 
 // Unnamed Factory Constructors
 
-//   - a generative constructor
-//   - does not have to return an instance of its type
+//   - a generative constructor which
+//     does not have to return an instance of its type
 
 // Generative Constructor Redirection:
 
-//   - can not redirect to factory constructors
+//   - can not redirect to a factory constructor using an initalizer list
 //   - Logger._internal(String name) : this(name);
+
+// Signature for LoggerCallback
+
+typedef LoggerCallback = Logger Function();
 
 class Logger {
   final String name;
 
   bool mute = false;
 
-  // static variables
-
-  //   - class variables
-
   static final _cache = <String, Logger>{};
 
-  factory Logger.fromJSON(Map<String, String> json) => Logger(
-        json['name'].toString(),
-      );
+  static Map<String, Logger> get cache => _cache;
 
-  factory Logger(String name) => _cache.putIfAbsent(
-        name,
-        () => Logger._internal(name),
-      );
+  // Factory Named Constructor (Identifier Constructor)
 
-// Named Generative Constructor
+  factory Logger.fromJSON(Map<String, String> json) {
+    final String result = json['name'].toString();
+    return Logger(result);
+  }
 
-//   - to initalize instance variables and create a type
+  // Factory Unnamed Constructor (Generator Constructor)
 
+  factory Logger(String name) {
+    final LoggerCallback handleLogger = () => Logger._internal(name);
+
+    return _cache.putIfAbsent(
+      name,
+      handleLogger,
+    );
+  }
+
+  // Default Named Constructor
   Logger._internal(this.name);
 
   void log(String msg) => !mute ? print(msg) : null;
+
+  @override
+  String toString() => 'Logger(name: $name)';
 }
+
+// Simulate fetching data from an API (Application Programming Interface)
 
 Future<Map<String, String>> useFetchLoggerApi() async {
   await Future.delayed(
@@ -61,13 +72,17 @@ Future<Map<String, String>> useFetchLoggerApi() async {
 }
 
 Future<void> main() async {
-  Logger logger = Logger('U.I.');
+  final Logger logger = Logger('U.I.');
 
-  logger.log('button clicked');
+  final Map<String, String> jsonData = await useFetchLoggerApi();
 
-  Logger loggerJson = Logger.fromJSON(
-    await useFetchLoggerApi(),
-  );
+  final Logger loggerJson = Logger.fromJSON(jsonData);
 
-  print('loggerJson name: ${loggerJson.name}');
+  print('\nAPI Data result: $jsonData\n');
+
+  logger.log('Logger.log: button clicked\n');
+
+  print('Logger class variable: ${Logger.cache}\n');
+
+  print('loggerJson name: ${loggerJson.name}\n');
 }
