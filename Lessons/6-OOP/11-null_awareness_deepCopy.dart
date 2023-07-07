814e9fc4335a6cdb357ec4a74a6287db8252b63c
Kwe <Kweayon@gmail.com>
2023-07-07 11:11:43 -0500

    Updated file names

--- Lessons/6-OOP/11-null_awareness_deepCopy.dart
+++ Lessons/6-OOP/11-null_awareness_deepCopy.dart
@@ -1,10 +1,20 @@
+// Custom Deep Copy Method: Maintaining Immutablility
+
+//   - You can implement your own deep copy method to maintain immutability
+//   - Allowing you to change some properties while retaining others
+//     without the need to reference the same addresses in memory
+//     returning a new object of that shape
+//     without the need to implement a stateful object
+
 class Credentials {
   const Credentials({this.email = '', this.password = ''});
   final String email;
   final String password;
 
   Credentials copyWith({String? email, String? password}) => Credentials(
-      email: email ?? this.email, password: password ?? this.password);
+        email: email ?? this.email,
+        password: password ?? this.password,
+      );
 
   @override
   String toString() => 'Credentials($email,$password)';
