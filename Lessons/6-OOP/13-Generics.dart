41a1f3ff4b0458f736e2062c87fb15b2e995727f
Kwe <Kweayon@gmail.com>
2023-07-08 00:50:39 -0500

    rrefactor: updated comments

--- Lessons/6-OOP/13-Generics.dart
+++ Lessons/6-OOP/13-Generics.dart
@@ -7,6 +7,8 @@
 //         arguments to your implementation can be of ANY type, including nullable
 //   - generics are reified meaning type information is retained at run-time
 
+// generic classs that is explicitly expecting one of three specified types
+
 class Point<T extends num, int, double> {
   final T x, y;
   const Point({
@@ -15,10 +17,21 @@ class Point<T extends num, int, double> {
   });
 }
 
-// generic classs that is explicitly expecting one of three specified types
+// the above class instance variables can be of any non-nullable type
+
+class Point2<T extends Object> {
+  final T x, y;
+  const Point2({
+    required this.x,
+    required this.y,
+  });
+}
 
 void main() {
   final Point p0 = Point(x: 4, y: 3.14);
+
+// the bellow code will through a run time error
+  // final Point2 p1 = Point2(x: null, y: '');
 
   // type information is retained at run-time
   print('\nx run time type: ${p0.x.runtimeType}\n');
