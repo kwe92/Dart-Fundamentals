0fffaf451abd75ba4dc42c57ec17501ad0f7926d
Kwe <Kweayon@gmail.com>
2023-07-07 19:32:35 -0500

    refactor: Refactored lesson and added new comments

--- Lessons/6-OOP/11-null_awareness_deepCopy.dart
+++ Lessons/6-OOP/11-null_awareness_deepCopy.dart
@@ -1,30 +1,69 @@
 // Custom Deep Copy Method: Maintaining Immutablility
 
 //   - You can implement your own deep copy method to maintain immutability
-//   - Allowing you to change some properties while retaining others
-//     without the need to reference the same addresses in memory
-//     returning a new object of that shape
-//     without the need to implement a stateful object
+//   - Allowing you to copy an object and all its properties
+//     without the need to reference the same address in memory
+//     returning a new object of that type with only
+//     the passed in properties changed
 
-class Credentials {
-  const Credentials({this.email = '', this.password = ''});
-  final String email;
-  final String password;
+// Null operator ??
 
-  Credentials copyWith({String? email, String? password}) => Credentials(
-        email: email ?? this.email,
-        password: password ?? this.password,
+//   - part of the Set of null aware operators
+//   - returns the expression to the left if it is not null
+//     otherwise returns the expression to the right
+
+class UserCredentials {
+  final String userName, userEmail, userPassword;
+  final bool isAdmin;
+
+  const UserCredentials({
+    required this.userName,
+    required this.userPassword,
+    required this.userEmail,
+    required this.isAdmin,
+  });
+
+  UserCredentials copyWith({
+    String? userName,
+    String? userPassword,
+    String? userEmail,
+    bool? isAdmin,
+  }) =>
+      UserCredentials(
+        userName: userName ?? this.userName,
+        userPassword: userPassword ?? this.userPassword,
+        userEmail: userEmail ?? this.userEmail,
+        isAdmin: isAdmin ?? this.isAdmin,
       );
 
   @override
-  String toString() => 'Credentials($email,$password)';
+  String toString() => """
+UserCredentials(
+  userName: ${this.userName},
+  userPassword: ${this.userPassword},
+  userEmail: ${this.userEmail},
+  isAdmin: ${this.isAdmin},
+  )
+""";
 }
 
 void main() {
-  const credentials = Credentials();
-  final update1 = credentials.copyWith(email: 'BakiHanma@anime.org');
-  final update2 = update1.copyWith(password: 'Baki');
-  print(credentials);
-  print(update1);
-  print(update2);
+  final user = UserCredentials(
+    userName: 'Gaara',
+    userPassword: 'SandKing',
+    userEmail: 'kingofsand@naruto.anime',
+    isAdmin: true,
+  );
+
+  final UserCredentials updatedUser = user.copyWith(
+    userPassword: '1TailedSandDemonKing',
+  );
+
+  final UserCredentials anotherUpdate = updatedUser.copyWith(userEmail: 'HiddenInTheSand@naruto.anime');
+
+  print('\nCurrent user credentials:\n\n$user\n');
+
+  print('\nFirst update user credentials:\n\n$updatedUser\n');
+
+  print('\nSecond update user credentials:\n\n$anotherUpdate\n');
 }
