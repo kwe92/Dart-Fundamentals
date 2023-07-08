f7f6ebdfa98ea1b5d5dfb6a4abd27b7799df44e4
Kwe <Kweayon@gmail.com>
2023-07-08 10:46:51 -0500

    udate: refactored lesson and comments to be accurate

--- Lessons/6-OOP/13-Generics.dart
+++ Lessons/6-OOP/13-Generics.dart
@@ -2,9 +2,11 @@
 //TODO:  type literals of all kinds are generics
 // ?? <String>[] Map<String, int>()
 
+import 'dart:math';
+
 // Generic Types
 
-//   - allows an implementation to specify multiple types implicitly or explicitly
+//   - allows an implementation to specify multiple types implicitly or explicitly by extends
 //   - uses a single capitalized letter to represent a placeholder type
 
 // Placeholder Type
@@ -18,30 +20,18 @@
 
 // Explicit Parameterized
 
-//   - you can explicitly define the range of types a placeholder type can be
-//   - this is achieved by suffixing the place holder type with extends followed by what types it can be
-//       - e.g. Type <T extends num, int, double>
-//       - similar to TypeScripts:
-//           type multiType = num || string
-
-// Implicit Parameterized Type
-
-//   - if extends is ommited e.g. <T>
-//     the placeholder type can be on ANY known type
-//     including nullable
+//   - you can explicitly define what type a placeholder type can be
+//   - this is achieved by suffixing the place holder type with extends followed by what type it can be
+//       - e.g. <T extends Object>
+//       - typically used to ensure an Object is non-null when generic
 
 //Generics Are reified
 
-//   - reified means type information is retained at run-time but not compile-time
-// tooling highlighting may not always show the corrent type
+//   - reified means type information is retained at run-time but not compile-time if the type is not parameterized when instantiated or a method is envoked
 
-// Explicit Generic Class Implementation Example
+// Generic Class Implementation Example
 
-//   - explicitly expecting one of three specified types
-
-import 'dart:math';
-
-abstract base class AbstractBasePoint<T extends num, int, double> {
+abstract base class AbstractBasePoint<T> {
   final T x, y;
   const AbstractBasePoint({
     required this.x,
@@ -49,38 +39,33 @@ abstract base class AbstractBasePoint<T extends num, int, double> {
   });
 
   T euclideanDistance(Point other);
+
+  @override
+  String toString() => 'Point($x, $y)';
 }
 
-final class Point<T extends num, int, double> extends AbstractBasePoint {
+final class Point<T> extends AbstractBasePoint {
   const Point({required super.x, required super.y});
 
   @override
   T euclideanDistance(Point other) {
-    final result = sqrt(
+    final T distance = sqrt(
       pow((other.x - this.x), 2) + pow((other.y - this.y), 2),
     ) as T;
-    return result;
+    return distance;
   }
 }
 
-// TODO: Create lesson for implicit
-// the above class instance variables can be of any non-nullable type
-
-class Point2<T extends Object> {
-  final T x, y;
-  const Point2({
-    required this.x,
-    required this.y,
-  });
-}
-
 void main() {
-  // final Point p0 = Point(x: 4, y: 3.14);
+  // parameterized type information is ommited
+  // so the compile-time type is Point<dynamic>
 
-// the bellow code will through a run time error
-  // final Point2 p1 = Point2(x: null, y: '');
+  final Point p0 = Point(x: 4, y: 3.14);
+  final Point p1 = Point(x: 3.124, y: 42);
+  final distance = p0.euclideanDistance(p1);
 
-  // type information is retained at run-time
-  // print('\nx run time type: ${p0.x.runtimeType}\n');
-  // print('y run time type: ${p0.y.runtimeType}\n');
+  // the reified run-time type information is retained
+  print('\nx run time type: ${p0.x.runtimeType}\n');
+  print('y run time type: ${p0.y.runtimeType}\n');
+  print('euclidean distance between:\n\n$p0 and $p1:\n\n${distance}\n');
 }
