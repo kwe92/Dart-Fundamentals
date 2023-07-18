e865b2c4d17d54f4877c5cfe74407d20336e2a8e
Kwe <Kweayon@gmail.com>
2023-07-18 10:12:18 -0500

    new lesson: working with List.generate

--- Lessons/1-Collections/1.1-collections_list_methods.dart
+++ Lessons/1-Collections/1.1-collections_list_methods.dart
@@ -1,2 +1,49 @@
 // TODO: Add a lesson on List.generate
 // TODO: Look at other list functions
+
+// Generating Lists
+
+//   - Dart provides an easy way to generate both
+//     growable lists and fixed length lists
+//   - via the static method List.generate
+
+// List.generate
+
+//   - .generate is a factory method that is used to
+//      generate N elements of a List
+//   - the first argument is the desired length of the List
+//     which is 0-base indexed (n - 1) in ascending order
+//   - values of the .generate function are
+//     handled by the second argument a callback
+//     in the shape of:
+//       - return_type Function(int index)
+
+// create the shape of our generator callback function
+
+typedef IntGeneratorCallback = int Function(int i);
+
+void main() {
+  final IntGeneratorCallback generatorCallback = (int index) => index * 2;
+
+  final List generatedList = List.generate(
+    12,
+    generatorCallback,
+  );
+
+  final List<int> fixedGeneratedList = List.generate(
+    9,
+    generatorCallback,
+    growable: false,
+  );
+
+  try {
+    // will throw an error as the list is not growable
+    fixedGeneratedList.add(3);
+  } catch (error, stackStrace) {
+    print('\nError: $error | error run-time type: ${error.runtimeType}\n');
+  } finally {
+    print('growable generated List $generatedList\n');
+    print('generated list type without specifying parameterized type ${generatedList.runtimeType}\n');
+    print('fixed generated List: $fixedGeneratedList\n');
+  }
+}
