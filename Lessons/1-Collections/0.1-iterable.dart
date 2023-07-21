be205536b8fa3178e8cca3fc3504cd3990a5ffdc
Kwe <Kweayon@gmail.com>
2023-07-21 17:01:55 -0500

    update: inital comments

--- Lessons/1-Collections/0.1-iterable.dart
+++ Lessons/1-Collections/0.1-iterable.dart
@@ -1,6 +1,20 @@
 // Accessing Elements of an Iterable Type
 
-//   - as stated earlier Iterable types can utilize
-//     bracket notation; Big-O(1) - constant time element access
-//   - elements of are access linearly Big-O(n)
+//   - as stated earlier Iterable objects can not utilize
+//     bracket notation; O(1) constant time element access (worst-case asymptotic time analysis)
+//   - elements of an Interable object are access linearly O(n)
+
+// Iterable.iterator
+
+//   - a read-only computed variable (immutable getter)
+//   - allows you to step through the elements of an Iterable in O(n)
+//   - returns a new interator when called
+
+// Iterable.moveNext
+
+//   - how the iterator steps through the Iterable
+//   -
+//   -
+//   -
+
 void main() {}
