1279ab29471e4b8628929d8ef7f8a361844c73e4
Kwe <Kweayon@gmail.com>
2023-07-23 10:16:17 -0500

    update: expanding comments in my own words

--- Lessons/1-Collections/0.1-iterable-iterator.dart
+++ Lessons/1-Collections/0.1-iterable-iterator.dart
@@ -4,7 +4,7 @@
 //     i.e. O(1) constant time element access (worst-case asymptotic time complexity)
 //   - elements of an Interable object are accessed linearly O(n)
 
-// Iterable.iterator
+// Iterable.iterator | property | getter computed variable
 
 //   - read-only computed variable (immutable getter variable)
 //   - getter is a derivation process;
@@ -15,16 +15,27 @@
 //     from which it was derived in Order-of(n) linear-time
 //   - returns a new Iterator object when called
 
-// Iterator.moveNext
+// Iterator.moveNext | abstract method
 
 //   - a predicate method (boolean function) of an Iterator object
 //   - if the predicate is evaluated to be true
 //   - the Iterator object is moved forward in the iteration
 //     i.e. sequence of current values "elements"
-//   - can be passed into a while loop and will be called
+//   - Iterator.current holds the current value identified by moveNext until moveNext is called again
+//   - Iterator.moveNext can be passed into a while loop and will be called
 //     continuously until the sequence is exhausted (moveNext returns false)
+//     in which case Iterator.current should not be read
 
-// How Iterable Instance Member Access Elements
+// Iterator.current | property | parameterized getter computed variable
+
+//   - the current element the Iterator is on
+//   - initally Null if Iterator.moveNext has not been called first
+//   - calling Iterator.current before calling Iterator.moveNext
+//     returns true will throw in an error
+//   - calling iterator.current after Interator.moveNext
+//     has returned false will generate unexpected values or throw
+
+// How Iterable Instance Members Access Elements
 
 //   - ALL members of an Iterable object
 //     access the elements in Order-of(n) linear time
