17cc3e2ecf36dfb53e1ba94fee1e7063198d3c9e
Kwe <Kweayon@gmail.com>
2023-07-25 12:04:58 -0500

    refactor: added comments and new code

--- Lessons/1-Collections/1-list.dart
+++ Lessons/1-Collections/1-list.dart
@@ -1,10 +1,29 @@
 // TODO: read docs on lists in dart and make a lesson
 
-// Iterable: List
+// List
 
+//   - An indexable collection of objects witha length
 //   - 0-based indexed array
-//   - one-to-one correspondence to Whole numbers {0, 1, 2, 3,...}
-//   - non-homogeneous by default
-//   - List literals [] can be declared as Interable
+//   - index has a one-to-one correspondence with Whole numbers {0, 1, 2, 3,...}
+//   - if type is omitted non-homogeneous by default i.e. <dynamic>[]
+//   - List literals <E>[] can be declared as Iterable
 
-void main() {}
+// Most Common List Implementations (sub-classes)
+
+//   Fixed-length List
+
+//     - The length is determined at compile-time and can not be changed
+//     - operations attempting to change the length by adding or removing elements will throw an error
+
+//   Growable List
+
+//     - the default implementation of a List literal <E>[]
+//     - an internal buffer is kept and grows as the list grows
+//     - allowing for amortized constant time operations
+void main() {
+  var arr = List.empty();
+
+  // arr[0] = 42;
+
+  print(arr);
+}
