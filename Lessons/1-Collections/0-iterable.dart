b6a1794542ec6d0d9a3cc13825f98253dac551dd
Kwe <Kweayon@gmail.com>
2023-07-25 11:20:59 -0500

    updated comments

--- Lessons/1-Collections/0-iterable.dart
+++ Lessons/1-Collections/0-iterable.dart
@@ -4,7 +4,7 @@
 //     extended or mixed in to implement its interface
 //   - the intended interface is a data structure, representing a collection of Objects;
 //     which are "elements" or "values" that can be accessed sequentially (linearly) O(n) worst case (asymptotic) time-complexity
-//   - can be declared and initalized with List and Set literals or literal parameterized types
+//   - can be declared and initalized with List and Set literals or parameterized literal
 //   - List and Set classes are sub-classes of Iterable; implementations of the Iterable super-class
 //   - if a List or Set literal is initalized as an Iterable it can't be indexed using brackets
 
