cba8684c245631486fc33903789f18140423605c
Kwe <Kweayon@gmail.com>
2023-07-27 16:45:12 -0500

    update: added function description

--- utility/spacedPrint.dart
+++ utility/spacedPrint.dart
@@ -1,3 +1,5 @@
+/// print statement with optional prefix_space and suffix_space boolean parameters.
+/// Allowing less bolierplate code when multiple print statements are required.
 void spacedPrint<T>(T obj, {bool prefix_space = false, bool suffix_space = true}) => prefix_space && !suffix_space
     ? print('\n$obj')
     : !prefix_space && suffix_space
