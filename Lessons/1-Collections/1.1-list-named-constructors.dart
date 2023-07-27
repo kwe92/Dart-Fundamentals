51910289494553e36fa534dae8661df880fea6c3
Kwe <Kweayon@gmail.com>
2023-07-27 17:17:51 -0500

    update: expanded lesson and omments | implemented List.castFrom

--- Lessons/1-Collections/1.1-list-named-constructors.dart
+++ Lessons/1-Collections/1.1-list-named-constructors.dart
@@ -1,26 +1,34 @@
+// ignore_for_file: unused_catch_stack
+
 import '../../utility/spacedPrint.dart';
 
-// Constructing a List using Named Constructors
+// Constructing a List: Named Constructors
 
 //   - as stated previously an alternative way to instantiate a List is by way of
 //     named "identity" constructors
-//   - most named constructors are Factory Constructors allowing for more complex logic when defining their implementation
-//     but can be Initalizer Lists for simpler implementations that do not need access to the this keyword
+//   - most named constructors are Factory Constructors
+//     allowing for more complex logic when defining their implementation
+//   - however named constructors can also be Initalizer Lists
+//     for simpler implementations that do not need access to the this keyword
 
 void main() {
   // List Named "Identity" Constructors
 
-  //List.filled | default fixed-length "length can not be changed at compile-time"| modifiable / mutable references "pointers in memory"
+  // List.filled
+
+  //   - default fixed-length "length can not be changed at compile-time"
+  //   -  modifiable / mutable i.e. references "pointers in memory" can be accessed and their elements changed
 
   final fixedArr0 = List<dynamic>.filled(5, 0);
 
   try {
-    // attempting to modify the length of a fixed-length List will result in a run-time error
+    // modifying the length of a fixed-length List will result in a run-time error
+
     fixedArr0.add('I can not be added');
   } catch (error, stackTrace) {
-    spacedPrint('Error: $error');
+    spacedPrint('Error: $error', prefix_space: true);
   } finally {
-    // The references "pointers" can be accessed to modify what object is being help at that point in memory
+    // references "pointers" can be accessed to modify the elements "objects" that are being held at that point in memory
 
     spacedPrint(fixedArr0);
 
@@ -28,4 +36,18 @@ void main() {
 
     spacedPrint(fixedArr0);
   }
+
+  // List.castFrom
+
+  //   - adapts the source List <S>[...] into a parameterized type <T>[...]
+  //   - the source type "data-type of elements" within <S>[...]
+  //     must be a sub-type of <T>[...] or an error is thrown
+
+// e.g. Casting an Array of doubles into an Array of num
+// as double is a subtype of num in the type heirarchy
+
+  final decimalArr = <double>[3.14, 0.1, 0.01, 0.001];
+  final List<num> castAsNumArr = List.castFrom<double, num>(decimalArr);
+
+  spacedPrint(castAsNumArr, prefix_space: true);
 }
