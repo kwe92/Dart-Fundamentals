ca195a06ca0d1ef70d0bffbb5370002c6fdbf242
Kwe <Kweayon@gmail.com>
2023-08-16 22:31:59 -0500

    update: added socket error response

--- AsyncProgramming/LocalEndpoints/task_service_test.dart
+++ AsyncProgramming/LocalEndpoints/task_service_test.dart
@@ -5,12 +5,18 @@ import 'dart:convert';
 
 import '../../utility/spacedPrint.dart';
 
+// TODO: refactor implementation to handle if the server is not reached | try catch error
+
+// TODO: abstract into a HttpService mixin and then create an ApiService to inherit from
+
 class TaskService {
   const TaskService._();
 
   // Todo: host should be hidden in .env | DotEnv dotenv | package:flutter_dotenv/src/dotenv.dart
 
   static const _host = "http://127.0.0.1:8082";
+
+  static final socketError = http.Response('[{"error": "there was an issue with requesting your data"}]', 550);
 
   static Future<List<dynamic>> get(String endpoint, {Map<String, String>? headers}) async {
     final response = await http.get(
@@ -22,7 +28,11 @@ class TaskService {
 
     final List<dynamic> data = jsonDecode(responseBody);
 
+    // final errorData = jsonDecode(socketError.body);
+
     return data;
+
+    // return errorData;
   }
 }
 
