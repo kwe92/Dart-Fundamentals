44e9ce72b801adb71c7540f2750052959db9b9fc
Kwe <Kweayon@gmail.com>
2023-08-19 23:30:15 -0500

    update: refactor services returns model

--- async_programming/anime_meme_service/services/ani_memes_service.dart
+++ async_programming/anime_meme_service/services/ani_memes_service.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
 import 'dart:io';
-import 'package:http/http.dart' as http;
 import '../../working_with_local_endpoints/services/api_service.dart';
+import '../models/ani_memes_model.dart';
 
 // TODO: handle when request fails, ping multiple times
 
@@ -11,7 +12,9 @@ class AniMemeService extends ApiService {
   @override
   Map<String, String> get headers => {HttpHeaders.contentTypeHeader: "application/json"};
 
-  Future<http.Response> getMeme() async {
-    return await get("/animeme");
+  Future<AniMemesModel> getMeme() async {
+    final response = await get("/animeme");
+    final aniMeme = AniMemesModel.fromJSON(jsonDecode(response.body));
+    return aniMeme;
   }
 }
--- async_programming/anime_meme_service/services/request_ani_memes.dart
+++ async_programming/anime_meme_service/services/request_ani_memes.dart
@@ -1,14 +1,11 @@
 // ignore_for_file: unused_catch_stack, unused_field
 
-import 'dart:convert';
 import '../../../utility/spacedPrint.dart';
-import '../models/ani_memes_model.dart';
 import 'ani_memes_service.dart';
 
 Future<void> main() async {
   final AniMemeService memeService = AniMemeService();
-  final response = await memeService.getMeme();
-  final aniMeme = AniMemesModel.fromJSON(jsonDecode(response.body));
+  final aniMeme = await memeService.getMeme();
 
   spacedPrint(
     'title: ${aniMeme.title} | image url: ${aniMeme.imgUrl}',
