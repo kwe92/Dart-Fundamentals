050727e4d1e0b6492cd4b601f63403efd14958a6
Kwe <Kweayon@gmail.com>
2023-08-31 06:36:54 -0500

    update: commented out delete tast

--- async_programming/working_with_local_endpoints/request_tasks.dart
+++ async_programming/working_with_local_endpoints/request_tasks.dart
@@ -1,9 +1,19 @@
+import 'models/TaskModel.dart';
 import 'services/task_service.dart';
 
 Future<void> main() async {
   final taskService = TaskService();
 
-  await taskService.getAllTasks();
+  final task0 = TaskModel.fromJson({
+    "id": "1001",
+    "taskName": "Be Efficient",
+    "taskDetails": "Aim for effieciency over quickness and you will find speed in effieciency as you will avoid many errors.",
+    "createdDate": "08-22-2023",
+  });
+
+  final allTasks = await taskService.getAllTasks();
+
+  print('\nAll Tasks Run Time Type: \n\n${allTasks.runtimeType}');
 
   await taskService.getTaskById(1005);
 
@@ -13,12 +23,9 @@ Future<void> main() async {
         "Moment by moment, rest in the void;\nThe universal mind;\nBurn away what is not pure;\nWhat is not born from love and compassion;\nFor oneself, for humanity as you are one.",
   });
 
-  await taskService.updateTask({
-    "id": "1001",
-    "taskName": "Be Efficient",
-    "taskDetails": "Aim for effieciency over quickness and you will find speed in effieciency as you will avoid many errors.",
-    "createdDate": "08-22-2023",
-  });
+  await taskService.updateTask(
+    task0.toJson(),
+  );
 
   await taskService.deleteTask(1002);
 
