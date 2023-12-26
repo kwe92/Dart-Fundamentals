73b8a8fe0f04bffa475332a6bacf422212eb29e3
Kwe <Kweayon@gmail.com>
2023-12-25 23:59:05 -0600

    feature: AbstractMaid class

---
+++ design_patterns_solid_principles/5-dependency_inversion_principle/abstract_maid.dart
@@ -0,0 +1,12 @@
+abstract class AbstractMaid {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String phoneNumber;
+  const AbstractMaid({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.phoneNumber,
+  });
+}
