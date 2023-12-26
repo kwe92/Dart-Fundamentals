2a9425d3a1bb66f70f0320196d2c0d297ef1a6e9
Kwe <Kweayon@gmail.com>
2023-12-26 00:00:03 -0600

    feature: AbstractOwner class

---
+++ design_patterns_solid_principles/5-dependency_inversion_principle/abstract_owner.dart
@@ -0,0 +1,14 @@
+abstract class AbstractOwner {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String phoneNumber;
+  final String Address;
+  const AbstractOwner({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.phoneNumber,
+    required this.Address,
+  });
+}
