87312c7f30df234407987e25f2760de1f1686441
Kwe <Kweayon@gmail.com>
2023-12-26 06:41:35 -0600

    feat: Owner Class

---
+++ design_patterns_solid_principles/5-dependency_inversion_principle/owner.dart
@@ -0,0 +1,12 @@
+// the only dependency is an abstraction (shared by the high level module)
+import 'abstract_owner.dart';
+
+class Owner extends AbstractOwner {
+  const Owner({
+    required super.firstName,
+    required super.lastName,
+    required super.email,
+    required super.phoneNumber,
+    required super.Address,
+  });
+}
