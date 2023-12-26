d7f5657b5c803a0d9ffe5c8c8d763b6630c6f5cd
Kwe <Kweayon@gmail.com>
2023-12-26 06:41:10 -0600

    feat: Maid Class

---
+++ design_patterns_solid_principles/5-dependency_inversion_principle/maid.dart
@@ -0,0 +1,11 @@
+// the only dependency is an abstraction (shared by the high level module)
+import 'abstract_maid.dart';
+
+class Maid extends AbstractMaid {
+  const Maid({
+    required super.firstName,
+    required super.lastName,
+    required super.email,
+    required super.phoneNumber,
+  });
+}
