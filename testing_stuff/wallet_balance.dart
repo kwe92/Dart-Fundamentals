5883eacb2c4f102bd6eb88321fa890b5d1e2fa80
Kwe <Kweayon@gmail.com>
2024-05-08 12:42:04 -0500

    feat: working through wallet ballance implementations

---
+++ testing_stuff/wallet_balance.dart
@@ -0,0 +1,22 @@
+import 'dart:math';
+
+import '../lessons/7-Extensions_Mixins/3-named_extensions_with_generics.dart';
+
+void main() {
+  List<double> roundedAmounts = randDoubleArr(9);
+
+  print("roundedAmounts: ${roundedAmounts.sum().toStringAsFixed(2)}");
+}
+
+List<double> randDoubleArr(int length) => [
+      for (int i = 0; i < length; i++)
+        () {
+          double changeToRoundUp = Random().nextDouble();
+
+          if (changeToRoundUp == 0) {
+            changeToRoundUp += 0.01;
+          }
+
+          return double.parse(changeToRoundUp.toStringAsPrecision(2));
+        }(),
+    ];
