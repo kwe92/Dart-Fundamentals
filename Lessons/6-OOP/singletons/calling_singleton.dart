8dda578822396f595e5163828a4239fbdd295e82
Kwe <Kweayon@gmail.com>
2024-08-12 09:35:47 -0500

    feat: updated comments for singleton

--- Lessons/6-OOP/singletons/calling_singleton.dart
+++ Lessons/6-OOP/singletons/calling_singleton.dart
@@ -1,14 +1,26 @@
 import 'singleton.dart';
 
-final instance00 = Counter();
-
-final instance01 = Counter();
-
 void main() {
-  for (var i = 0; i < 5; i++) {
-    instance00.increment();
-    print("increment the count of instance00");
-  }
+  try {
+    final instance00 = Counter(4);
 
-  print("instance01 total count: ${instance01.count}");
+    final instance01 = Counter();
+
+    for (var i = 0; i < 5; i++) {
+      instance00.increment();
+      print("increment the count of instance00");
+    }
+
+    print("instance00 total count: ${instance00.count}");
+
+    print("instance01 total count: ${instance01.count}");
+
+    print(instance00.count == instance01.count);
+
+    print(instance00.hashCode == instance01.hashCode);
+  } catch (e, st) {
+    print(e.toString());
+
+    print(st.toString());
+  }
 }
--- Lessons/6-OOP/singletons/singleton.dart
+++ Lessons/6-OOP/singletons/singleton.dart
@@ -1,13 +1,54 @@
-class Counter {
-  int _count = 0;
+class InitialzationError extends Error {
+  final String message;
 
+  InitialzationError([this.message = "Error: Initial count has already been set."]);
+
+  @override
+  String toString() {
+    return message;
+  }
+}
+
+/// Represents a mutable counter that can be incremented and decremented
+class Counter {
+  // mutable rep
+  int _count;
+
+  // observer operation
   int get count => _count;
 
-  static final Counter _counter = Counter._internal();
+  static Counter? _singleton;
 
-  factory Counter() => _counter;
+  // creator operation
+  factory Counter([int? initialCount]) {
+    if (initialCount != null && _singleton != null) {
+      throw InitialzationError();
+    }
+    _singleton ??= Counter._internal(initialCount);
 
-  Counter._internal();
+    return _singleton!;
+  }
 
+  // creator operation
+  Counter._internal(int? initialCount) : _count = initialCount ?? 0;
+
+  // mutator operation
   void increment() => _count++;
+
+  // mutator operation
+  void decrement() => _count > 0 ? _count-- : null;
 }
+
+// Singleton
+
+//   - a creational design pattern ensuring that there is only one instance of a class with global access
+
+// How Singletons Are Implemented
+
+//   - the default constructor will be hidden from user
+
+//   - a private named constructor will be implemented
+
+//   - the class instance will be stored in a static variable
+
+//   - a static creation method / factory method will be implemented that returns the instance (cahced object in the static field)
