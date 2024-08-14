d2f3bc740c83b68fa56d54f1b57e901370275127
Kwe <Kweayon@gmail.com>
2024-08-14 09:40:05 -0500

    refact: cleaned up code and comments

--- async_programming/working_with_streams/streams.dart
+++ async_programming/working_with_streams/streams.dart
@@ -1,7 +1,38 @@
+/// creates a stream that outputs integers with a delay inbetween each output
+Stream<int> countStream(int n, [int? delayInSeconds]) async* {
+  for (var i = 0; i < n; i++) {
+    await Future.delayed(Duration(seconds: delayInSeconds ?? 1));
+    print("stream of data: $i");
+    yield i;
+  }
+}
+
+Future<num> sumStream(Stream<num> stream) async {
+  num result = 0;
+  await for (var ele in stream) {
+    result += ele;
+  }
+  return result;
+}
+
+Future<int> sumStream2(Stream<int> stream) => stream.reduce((previous, element) => previous + element);
+
+Future<int> main() async {
+  // final stream = Stream.fromIterable([1, 2, 3, 4, 5]);
+
+  final stream2 = countStream(5);
+
+  final sumOfStream = await sumStream2(stream2);
+  print("Sum of stream sequence: $sumOfStream");
+
+  return 0;
+}
+
 // Asynchronous Stream
 
-//  - a "Stream" is a sequence of asynchronous events
-//  - a "Stream" can be conidered an asynchronous iterable
+//  - a "Stream" is a sequence of asynchronous events i.e. list of Futures
+
+//  - a "Stream" can be considered an asynchronous iterable i.e. list of Futures
 
 // Create and add values to a steam:
 
@@ -12,66 +43,24 @@
 //   ~ Stream generator(async*, yield)
 
 //       - stream generator function
-//       - a function that uses keywords async* aor yeild
+
+//       - a function that uses keywords async* or yeild
 
 //   ~ StreamControler
 
 //       - more granular approach to how items are added to a stream
-
-Future<num> sumStream(Stream<num> stream) async {
-  num result = 0;
-  await for (var ele in stream) {
-    result += ele;
-  }
-  return result;
-}
 
 // Stream.reduce
 
 //    - Works like the Iterable.reduce
 //    - awaits every event to be available without explicitly being told by the user
 
-Future<int> sumStream2(Stream<int> stream) => stream.reduce((previous, element) => previous + element);
-
 // Stream Generators
 
 //    - similar to the synta of creating functions thar return a future
+
 //    - use "async*" prefix body rather than "async" keyword prefix for the body
-//    - use "yeild" instead of "await"
+
+//    - use "yeild*" instead of "yeild"
+
 //    - returns a Stream rather than a Future
-
-Stream<int> countStream(int n) async* {
-  for (var i = 1; i <= n; i++) {
-    await Future.delayed(Duration(seconds: 1));
-    print("squenced stream of data: $i");
-    yield i;
-  }
-}
-
-// sync* generator
-Iterable<num> syncGenerator(int n) sync* {
-  for (var i = 1; i <= n; i++) {
-    yield i;
-  }
-}
-
-Future<int> main() async {
-  // final stream = Stream.fromIterable([1, 2, 3, 4, 5]);
-
-  final stream2 = countStream(5);
-  final sumOfStream = await sumStream2(stream2);
-  print("Sum of stream sequence: $sumOfStream");
-  return 0;
-}
-
-// Iterable vs Stream - Similarities
-
-//   - Iterable and Stream type / class have many similarites
-//   - they both can be iteratied over with a for loop
-//   - they can be created with generator functions
-//   - they share similar methods / functions to proccess and modify items
-
-// Iterable vs Stream differences
-
-//   - Iterables are synchronous
-//   - Streams are asynchronous
