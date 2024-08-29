5d3c858f22df93953587bdaa245fdc16499aaea1
Kwe <Kweayon@gmail.com>
2024-08-29 13:49:45 -0500

    refact: comment edit and added more examples

--- Lessons/0-Variables_Operators_Enums_Loops/1-variables.dart
+++ Lessons/0-Variables_Operators_Enums_Loops/1-variables.dart
@@ -1,8 +1,6 @@
 // ignore_for_file: unused_local_variable
 
 void main() {
-// Specifiying a type without a keyword creates a mutable variable of that type. it is a compile-time error to try to assign a different type
-
   String fighter = 'Baki';
 
   int numFightingStyles = 6;
@@ -11,38 +9,22 @@ void main() {
 
   bool grappler = true;
 
-  // var keyword
-
-  //   - mutable variable
-
-  //   - can be initalized to any type and remains that type at runtime
-
   var spiritAnimal = 'Stich';
-
-  // final and const keywords:
-
-  //   final
-
-  //     - immutable variables that can only be set once and then becomes frozen
-  //        at compile time
-
-  //   const
-
-  //     - compile-time constant frozen at compile-time
 
   final int x = 2;
 
   const int y = 3;
 
-// String interpolation
+  dynamic changingVariable = 'I am a string!';
 
-//  - Insert variables into strings
+  print('using String operations on a dynamic variable: ${changingVariable.contains('a')}');
 
-//  - use $variableName syntax to format a single variable into a string literal.
+  changingVariable = 42;
 
-//  - Use ${} when you are specifiying more than one variable or using dot notation.
+  print('using integer operations on a dynamic variable: ${changingVariable.toDouble()}');
 
   print("$fighter is a fighter with $numFightingStyles fighting styles.");
+
   print(spiritAnimal);
 
   spiritAnimal = "Ifrit";
@@ -73,6 +55,7 @@ Dart language: Built-in Types
   - Record
   - Rune
   - Symbol
+  - dynamic
 
 Numbers
 
@@ -92,10 +75,67 @@ Numbers
 Strings
 
     - sequence of characters
+
     - A Dart string is a sequence of UTF-16 code units. 
 
 Boolean
 
-  - true and false values.
+  - true and false values (truth values).
 
 */
+
+// Type Declarations Without Keywords
+
+//   - Specifiying a type declaration without a keyword
+//     creates a reassignable reference (variable) of that type
+
+//   - it is a compile-time error to try to assign a different type
+
+// dynamic Type Declaration
+
+//   - a reassignable variable whose value can be of any type
+
+//   - the type of the value can change at compile-time without error
+
+//   - has no operations at compile-time
+
+//   - can use any arbitrary operation at compile-time, but at run-time
+//     if the variable does not refer to a value of the correct type then an error is thrown
+
+// var keyword
+
+//   - mutable (reassignable) variable (reference)
+
+//   - can be initalized to any type and remains that type at runtime
+
+
+// final and const keywords:
+
+//   final
+
+//     - immutable (unreassignabe) variable (reference) 
+
+//     - does not have to be initialized with a value upon declaration
+
+//     - but can only be initialized once and then becomes frozen at compile time
+
+//     - it is a compile-time error to try to use a final variable that has not been initialized
+//       unless it is prefixed with the late keyword
+
+//   const
+
+//     - compile-time constant frozen at compile-time
+
+//     - immutable (unreassignabe) variable (reference)
+
+//     - static checking for unreassignable references
+
+//     - must be initialized upon declaration
+
+// String interpolation
+
+//  - Insert variables (references) into strings
+
+//  - use $variableName syntax to format a single variable into a string literal.
+
+//  - Use ${} when you are specifiying more than one variable or using dot notation.
