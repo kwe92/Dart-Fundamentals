69a12e6df117e94caef9f0fbea3f7566800cc7a3
Kwe <Kweayon@gmail.com>
2024-08-29 10:28:08 -0500

    refact: updated comments and computeHailstoneSequenceRecursive implementation

--- Algorithms/hailstone_sequnece.dart
+++ Algorithms/hailstone_sequnece.dart
@@ -1,35 +1,40 @@
 void main() {
-  print(computeHailstoneSequenceRecursive(7));
+  print(computeHailstoneSequenceRecursive(-1));
 }
 
 /// Computes the hailstone sequence recursively.
 /// Assumes that the value of n > 0 (Possitive Integer / Natural Number).
 /// Returns hailstone sequence starting with n and ending with 1.
 List<int> computeHailstoneSequenceRecursive(int n) {
-  // initial empty sequence
+  if (n <= 0) {
+    throw Exception(' The value of n must be a positive integer, value passed in was $n');
+  }
+  // initial empty sequence / reassignable reference and mutable object you wish the closure to keep track of
   List<int> seq = [];
 
   // add initial element to the List
   seq.add(n);
 
-  // a recursive closure that computes the hailstone sequence until n equals 1
-  _innerRecursiveClosure(int n, List<int> seq) {
-    // terminating condition
+  // a recursive closure that computes the hailstone sequence until n == 1 and then returns the recurrence relation solution
+  List<int> _innerRecursiveClosure(int n, List<int> seq) {
+    // terminating condition, if n == 1 stop the computation and return the sequence
     if (n == 1) {
       return seq;
     }
 
+    // n is even
     if (n % 2 == 0) {
       n = n ~/ 2;
       seq.add(n);
-      _innerRecursiveClosure(n, seq);
-    } else {
-      n = 3 * n + 1;
-      seq.add(n);
-      _innerRecursiveClosure(n, seq);
+      return _innerRecursiveClosure(n, seq);
     }
 
-    return seq;
+    // n is odd
+    else {
+      n = 3 * n + 1;
+      seq.add(n);
+      return _innerRecursiveClosure(n, seq);
+    }
   }
 
   return _innerRecursiveClosure(n, seq);
@@ -39,16 +44,20 @@ List<int> computeHailstoneSequenceRecursive(int n) {
 /// Assumes that the value of n > 0 (Possitive Integer / Natural Number).
 /// Returns hailstone sequence starting with n and ending with 1.
 List<int> computeHailstoneSequence(int n) {
+  if (n <= 0) {
+    throw Exception(' The value of n must be a positive integer, value passed in was $n');
+  }
+
   List<int> seq = [];
 
-  // while n not equal to 1 mutate n depending on if n is evenly divisible by 2 | //?? NOTE: if you added the final keyword prefixing a parameter type that parameter can only be set once within a function e.g. (final int n)
+  // while n not equal to 1 mutate (reassign) n depending on if n is evenly divisible by 2 | //?? NOTE: if you added the final keyword prefixing a parameter type that parameter can only be set once (immutable / unreassignable) within a function e.g. (final int n), otherwise it is a reassignable local variable.
   while (n != 1) {
     print('mutable parameter value n: $n');
 
     // add element to List
     seq.add(n);
 
-    // mutate n
+    // mutate (reassign) n
     if (n % 2 == 0) {
       n = n ~/ 2;
     } else {
@@ -70,8 +79,8 @@ List<int> computeHailstoneSequence(int n) {
 
 //   - Sequences of integers generated in the Collatz problem.
 
-//   - a recurrence relation over the positive integers were the sequence elements may bounce up and down before eventually 
-//     ending in 1 and falling into the small loop of [4, 2, 1]
+//   - a recurrence relation over the positive integers were the sequence elements may bounce up and down
+//     before eventually ending in 1 and falling into the small loop of [4, 2, 1]
 
 //   - Sequence {a_n}
 
@@ -96,4 +105,4 @@ List<int> computeHailstoneSequence(int n) {
 
 //   - the hailstone sequence got its name from the bouncing up and down of the integer elements before
 //     finally ending in 1 and falling into the loop of [4, 2, 1], similar to how hailstones form in clouds
-//     bouncing up and down until they eventually fall to the earth after building enough weights
+//     bouncing up and down until they eventually fall to the earth after building enough weight
