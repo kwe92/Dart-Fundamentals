b8df09255ca7161bcaa16deafadd8a4d3d523824
Kwe <Kweayon@gmail.com>
2024-09-22 22:56:06 -0500

    refact: updated lesson and comments

--- Lessons/0-Variables_Operators_Enums_Loops/4.0-if_statement.dart
+++ Lessons/0-Variables_Operators_Enums_Loops/4.0-if_statement.dart
@@ -1,34 +1,16 @@
 import 'dart:math';
 
-// dart:math library:
-
-//  - Mathematical constants and functions
-//  - random number generator
-
 void main() {
-  // proposition
-
-  //   - a statement || assertion that is true or false
-  //   - a function returning true or false given some condition
-
-  bool proposition(num x, num y) => x > y ? true : false;
-
-  // Random Class
-
-  //   - generator of a random number
+  /// a propositional function (predicate) predicated on the values of x and y.
+  bool propositionalFunction(num x, num y) => x > y ? true : false;
 
   final num x = Random().nextInt(10);
+
   final y = Random().nextInt(10);
 
   final z;
 
-  // if-else statements
-
-  //   - evaluates a proposition
-  //   - if the proposition is true the if block is executed
-  //   - if the proposition is false for all if statements the else block is executed
-
-  if (proposition(x, y)) {
+  if (propositionalFunction(x, y)) {
     z = pow(x, 2);
   } else {
     z = pow(x, 3);
@@ -36,27 +18,13 @@ void main() {
 
   print('value of z: $z');
 
-  // Conditional Operators
-
-  //   - A concise way to evalute expressions
-
-  // Ternary operator
-
-  //   - Short hand for writing if-else statements
-  //   - syntax: condition ? exp1 : exp2;
-
-  // Binary operator
-
-  //   - If the first expression is null return the second expression
-  //   - syntax: exp1 ?? exp2
-
   Function rand = Random().nextInt;
 
   var a = rand(10);
 
   var b = rand(10);
 
-  // if a is greater than b assign c a squared else assign c b cubed.
+  // if a is greater than b assign c = a squared else assign c = b cubed.
 
   int c = a > b ? pow(a, 2).toInt() : pow(b, 3).toInt();
 
@@ -65,6 +33,7 @@ void main() {
   print('value of c: $c');
 
   a = null;
+
   b = rand(100);
 
   c = a ?? b;
@@ -73,3 +42,50 @@ void main() {
   print('value of b: $b');
   print('value of c: $c');
 }
+
+  // Proposition
+
+  //   - a statement or assertion that is true or false, typically a sentance
+
+  // Propositional Function (predicate / predicate function)
+
+  //   - a function predicated on the values of a propistional statement
+  //     the arguments passed to a propistional functions parameters
+
+  //   - a propsition with variables
+
+  // if-else statements
+
+  //   - control flow statements that evaluate a propositional function and run a branch of code
+
+  //   - if the propositional function is true the if block branch of code is executed
+
+  //   - if the propositional function is false for all if statements the else block branch of code is executed
+
+  // Conditional Operators
+
+  //   - A concise way to evalute boolean expressions
+
+  // Ternary operator
+
+  //   - Short hand for writing if-else statements
+
+  //   - syntax: condition ? exp1 if true : exp2 if false;
+
+  // Binary operator
+
+  //   - If the first expression is null return the second expression
+
+  //   - syntax: exp1 ?? exp2
+
+  //   - symantics: if expression 1 is null, then return expression 2
+
+// dart:math library:
+
+//  - Mathematical constants and functions
+
+//  - random number generator
+
+// Random Class
+
+  //   - generator of a pseudo random number
