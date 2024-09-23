a89bf090b1a10df8384d727fd15267e9f80ad637
Kwe <Kweayon@gmail.com>
2024-09-22 22:56:20 -0500

    refact: updated lesson and comments

--- Lessons/0-Variables_Operators_Enums_Loops/4.1-if_case_statement.dart
+++ Lessons/0-Variables_Operators_Enums_Loops/4.1-if_case_statement.dart
@@ -1,11 +1,3 @@
-// if case statements
-
-//   - Dart if statements now support if clauses followed by a pattern
-//   - A way to match the destructure ]of an object to a single pattern
-
-// when gaurd clause
-
-//   - used to evaluate a arbitrary boolean expression
 class Point {
   final double x;
   final double y;
@@ -18,25 +10,42 @@ class Point {
   String toString() => "Point($x, $y)";
 }
 
-const List<double> pair = [4.0, 2.0];
-const List<double> triplet = [2.12, 43.3, 5.6];
+const (double, double) pair = (4.0, 2.0);
 
-Point? getPoint(Iterable pair) {
-  if (pair case [double x, double y] when x > y && y > 0) {
+const (double, double, double) triplet = (2.12, 43.3, 5.6);
+
+Point? point(Record point) {
+  if (pair case (double x, double y) when x > y && y > 0) {
     return Point(x: x, y: y);
+  } else {
+    return null;
   }
-  return null;
 }
 
 void main() {
-  final Point? p1 = getPoint(pair);
-  final Point? p2 = getPoint(triplet);
+  final Point? p1 = point(pair);
+
+  final Point? p2 = point(triplet);
 
   print('Point 1: $p1');
   print('Point 2: $p2');
 }
 
+// if case statements
+
+//   - Dart if statements with case clauses followed by a pattern
+
+//   - A way to match and destructuring an object against a single pattern
+
+// when gaurd clause
+
+//   - used to evaluate an arbitrary boolean expression
+//     after a case clause
+
 // Summary
 
-// - patternsallow  you to use case clauses in if-statements
-// - If the pattern matches the value then the branch executes with any variables the pattern defines in scope
+//   - patterns allow you to use case clauses in if-statements
+
+//   - If the pattern matches the value then the branch executes with any variables the pattern defines in scope
+
+//   - you can add a guard clause to evaluate boolean expressions against a case clause after matching
