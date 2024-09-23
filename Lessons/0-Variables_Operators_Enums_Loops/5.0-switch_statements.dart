46825a1abe2c1e9801ea9561e41e950a4d23b0a4
Kwe <Kweayon@gmail.com>
2024-09-23 09:17:56 -0500

    refact: updated lesson

--- Lessons/0-Variables_Operators_Enums_Loops/5.0-switch_statements.dart
+++ Lessons/0-Variables_Operators_Enums_Loops/5.0-switch_statements.dart
@@ -1,76 +1,95 @@
-// Switch statements
-
-//   - evaluates an expression against a series of cases
-//   - each case clause is a pattern that a value can match against
-//   - if the value matches the case's pattern the case body executes
-//   - if no case clause is matched then the default case is returned
-//   - non-empty case clauses do not require a break statement as they jump to the end of the switch statement
-
-// Example 1:
-
-//  - given some mathmatical operation to perform
-//  - execute the given operation on variables x & y respectively
+enum ArithneticOperations {
+  add,
+  subtract,
+  multiply,
+  divide,
+  mod;
+}
 
 void main() {
   int x = 5;
   int y = 3;
 
-  List<String> operations = [
-    'add',
-    'subtract',
-    'multiply',
-    'divide',
-    'mod',
-  ];
+  final operation = ArithneticOperations.multiply;
 
-  switch (operations[2]) {
-    case 'add':
-      print('$x + $y = ${x + y}');
-    case 'subtract':
-      print('$x - $y = ${x - y}');
+// Example 1:
 
-    case 'multiply':
-      print('$x * $y = ${x * y}');
+//   - given some arithmetic operation to perform
 
-    case 'divide':
-      print('$x / $y = ${x / y}');
+//   - execute the given operation on the values referenced by the variables x and y respectively
+  try {
+    switch (operation) {
+      case ArithneticOperations.add:
+        print('$x + $y = ${x + y}');
 
-    case 'mod':
-      print('$x % $y = ${x % y}');
+      case ArithneticOperations.subtract:
+        print('$x - $y = ${x - y}');
 
-    default:
-      print('Incorrect operation, please use one of the following: add, subtract, multiply, divide, mod');
+      case ArithneticOperations.multiply:
+        print('$x * $y = ${x * y}');
+
+      case ArithneticOperations.divide:
+        print('$x / $y = ${x / y}');
+
+      case ArithneticOperations.mod:
+        print('$x % $y = ${x % y}');
+
+      default:
+        throw Exception('Incorrect operation, please use one of the following: add, subtract, multiply, divide, or mod');
+    }
+  } catch (err, _) {
+    print(err.toString());
   }
 
   // Example 2:
 
-  //      - Given an integer between 1-7 what is the corresponding day of the week
+  //   - Given an integer between 1-7 what is the corresponding day of the week
 
   int numericDayOfWeek = DateTime.wednesday;
 
-  switch (numericDayOfWeek) {
-    case 1:
-      print('Monday');
-      break;
-    case 2:
-      print('Tuesday');
-      break;
-    case 3:
-      print('Wednesday');
-      break;
-    case 4:
-      print('Thursday');
-      break;
-    case 5:
-      print('Friday');
-      break;
-    case 6:
-      print('Saturday');
-      break;
-    case 7:
-      print('Sunday');
-      break;
-    default:
-      print('Integer to be evaluated must be bewteen 1-7.');
+  try {
+    switch (numericDayOfWeek) {
+      case 1:
+        print('Monday');
+        break;
+      case 2:
+        print('Tuesday');
+        break;
+      case 3:
+        print('Wednesday');
+        break;
+      case 4:
+        print('Thursday');
+        break;
+      case 5:
+        print('Friday');
+        break;
+      case 6:
+        print('Saturday');
+        break;
+      case 7:
+        print('Sunday');
+        break;
+      default:
+        throw Exception('Integer to be evaluated must be bewteen 1-7.');
+    }
+  } catch (err, _) {
+    print(err.toString());
   }
 }
+
+// Switch statements
+
+//   - evaluates a value expression against a series of cases
+
+//   - each case clause is a pattern that a value can match against
+
+//   - if the value matches the cases pattern the case body executes
+
+//   - if no case clause is matched then the default case is returned
+
+//   - non-empty case clauses do not require a break statement as they jump to the end of the switch statement
+
+//   - empty case clauses fall through to the next case
+
+//   - for more complex control flow you can use case labels along with the continue keyword
