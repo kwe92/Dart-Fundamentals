30381d762101e61c0c49a29e39ba52144b928be3
Kwe <Kweayon@gmail.com>
2024-09-23 21:03:17 -0500

    refact: simplified lesson and made edits to comments

--- Lessons/0-Variables_Operators_Enums_Loops/5.5-switch_statements_guard_clause.dart
+++ Lessons/0-Variables_Operators_Enums_Loops/5.5-switch_statements_guard_clause.dart
@@ -1,10 +1,20 @@
-// Switch Statement / Expressions: Guard Clause
+void main() {
+  try {
+    const Pair pair0 = (3, -7);
 
-// when keyword
-//   - optional guard clause after a case clause is matched
-//   - evaluates an arbitrary boolean expression
-//   - adds additional constraints
-//   - false gaurd clauses fallthrough
+    final Point p0 = point(pair0);
+
+    print('p0: $p0');
+  } catch (err, _) {
+    print(err.toString());
+  } finally {
+    const Pair pair1 = (4.0, 2.0);
+
+    final Point p1 = point(pair1);
+
+    print('Point1: $p1');
+  }
+}
 
 class Point {
   final double x;
@@ -14,64 +24,26 @@ class Point {
     required this.y,
   });
 
-  void euclideanDistance() {}
-
   @override
   String toString() => 'Point($x, $y)';
-
-  @override
-  bool operator ==(Object other) => identical(this, other) ? true : other is Point && other.x == x && other.y == y;
-
-  @override
-  int get hashCode => x.hashCode ^ y.hashCode;
 }
 
-// function returning a switch expression
-Point getPoint(Pair pair) => switch (pair) {
-      (double a, double b) when a >= b && b > 0 => Point(x: a, y: b),
-      _ => throw PointException(
-          message: 'x must be greater than y and y must be greater than 0.',
-        ),
-    };
-
-// type signature
 typedef Pair = (double a, double b);
 
-// custom exception
-class PointException extends Error {
-  final String message;
-  PointException({required this.message});
+Point point(Pair pair) => switch (pair) {
+      (double a, double b) when a >= b && b > 0 => Point(x: a, y: b),
+//                         ^^^^^^^^^^^^^^^^^^^^ gaurd clause
+      _ => throw Exception('x and y must be positive integers and x must be greater than y.'),
+    };
 
-  @override
-  String toString() => message;
-}
+//Guard Clause
 
-void main() {
-  // try, catch, finally to handle error
-  try {
-    const Pair pair0 = (3, -7);
-    final Point p0 = getPoint(pair0);
-    print('p0: $p0');
-  } catch (errorMsg, stackTrace) {
-    print('\n$errorMsg\n');
-    printStackTrace(stackTrace);
-  } finally {
-    const Pair pair1 = (4.0, 2.0);
-    final Point p1 = getPoint(pair1);
-    print('Point1: $p1\n');
-  }
-}
+//   - optional guard clause can be set after a case clause
 
-// Prints Stack Trace of a call sequence triggering an exception
+//   - can be used anywhere case clauses are used (if case, switch statements, and switch expressions)
 
-void printStackTrace(StackTrace stackTrace) => stackTrace
-    .toString()
-    .split('#')
-    .where(
-      // filter predicate
-      (String stackTraceEle) => stackTraceEle.trim().length > 0,
-    )
-    .forEach(
-      // for each stack trace message print the message
-      (String stackTraceEle) => print('#${stackTraceEle.trim()}\n'),
-    );
+//   - evaluates an arbitrary boolean expression
+
+//   - adds additional constraints on case body execution
+
+//   - false gaurd clauses fallthrough, continuing to the next case instead of exiting the switch
