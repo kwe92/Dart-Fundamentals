06778350b1f7e9e23ab7e16d6915192a0407a528
Kwe <Kweayon@gmail.com>
2024-09-23 20:48:50 -0500

    refactored lesson

--- Lessons/0-Variables_Operators_Enums_Loops/5.1-switch_statements_cont.dart
+++ Lessons/0-Variables_Operators_Enums_Loops/5.1-switch_statements_cont.dart
@@ -1,5 +1,38 @@
 import 'dart:math';
 
+enum ExecutableCommands {
+  OPEN,
+  CLOSED,
+  PENDING,
+  APPROVED,
+  DENIED;
+}
+
+void main() {
+  final commands = ExecutableCommands.values;
+
+  for (var i = 0; i < commands.length; i++) {
+    final int randInt = _randInt(commands.length);
+
+    switch (commands[randInt]) {
+      case ExecutableCommands.OPEN:
+        executeOpen();
+      case ExecutableCommands.CLOSED:
+        executeClosed();
+      case ExecutableCommands.PENDING:
+        executePending();
+      case ExecutableCommands.APPROVED:
+        executeApproved();
+      case ExecutableCommands.DENIED:
+        executeDenied();
+      default:
+        executeUnknown();
+    }
+  }
+}
+
+int _randInt(int x) => Random().nextInt(x);
+
 void executeOpen() => print('Status is open.');
 
 void executeClosed() => print('Status is closed.');
@@ -12,35 +45,8 @@ void executeDenied() => print('Status is denied.');
 
 void executeUnknown() => print('Status is unknown.');
 
-int _randInt(int x) => Random().nextInt(x);
 
-void main() {
-  const List<String> commands = [
-    'OPEN',
-    'CLOSED',
-    'PENDING',
-    'APPROVED',
-    'DENIED',
-  ];
+// Default / Wildcard _ Clause
 
-  final int len = commands.length;
-
-  for (var i = 0; i < len; i++) {
-    final int randInt = _randInt(len);
-
-    switch (commands[randInt]) {
-      case 'OPEN':
-        executeOpen();
-      case 'CLOSED':
-        executeClosed();
-      case 'PENDING':
-        executePending();
-      case 'APPROVED':
-        executeApproved();
-      case 'DENIED':
-        executeDenied();
-      default:
-        executeUnknown();
-    }
-  }
-}
+//   - used at the end of a switch statement when that value expression
+//     does not match a case clause pattern
