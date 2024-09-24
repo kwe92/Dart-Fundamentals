67a26d3311912561a085fd4eaa44fb11edd50717
Kwe <Kweayon@gmail.com>
2024-09-24 12:06:29 -0500

    refact: updated lesson and comments

--- Lessons/0-Variables_Operators_Enums_Loops/6.0-enum.dart
+++ Lessons/0-Variables_Operators_Enums_Loops/6.0-enum.dart
@@ -8,38 +8,44 @@ enum DayOfWeek {
   Sunday,
 }
 
-/// The entry point to our program.
+abstract class CustomEnum implements Enum {
+  // void printValues();
+}
+
+enum CustEnum implements CustomEnum {
+  Day,
+}
+
 void main() {
-  // variables can be initalized from an enumerated list
   const DayOfWeek dow = DayOfWeek.Sunday;
 
   switch (dow) {
     case DayOfWeek.Monday:
       _getDayByNumber(dow);
-      print('Monday');
+      print(DayOfWeek.Monday.name);
     case DayOfWeek.Tuesday:
       _getDayByNumber(dow);
-      print('Tuesday');
+      print(DayOfWeek.Tuesday.name);
 
     case DayOfWeek.Wednesday:
       _getDayByNumber(dow);
-      print('Wednesday');
+      print(DayOfWeek.Wednesday.name);
 
     case DayOfWeek.Thursday:
       _getDayByNumber(dow);
-      print('Thursday');
+      print(DayOfWeek.Thursday.name);
 
     case DayOfWeek.Friday:
       _getDayByNumber(dow);
-      print('Friday');
+      print(DayOfWeek.Friday.name);
 
     case DayOfWeek.Saturday:
       _getDayByNumber(dow);
-      print('Saturday');
+      print(DayOfWeek.Saturday.name);
 
     case DayOfWeek.Sunday:
       _getDayByNumber(dow);
-      print('Sunday');
+      print(DayOfWeek.Sunday.name);
 
     default:
       print('Not a valid day of the week!');
@@ -51,10 +57,34 @@ void _getDayByNumber(DayOfWeek dow) => print(
     );
 
 
-// Enumerated types: enum - Enumerations
+// Enumerated types: Enumerations - enums
 
-//   - a set of unique constant values, defined by a special class
+//   - a special class representing a fixed set of unique constant values
 
+//   - in dart enums automatically extend the Enum class under the hood
+
+//   - enums are also sealed, meaning they cannot be implemented, mixedin, subclassed, or instantiated
+
+//   - abstract classes and mixin's can implements Enum but must then be implemented by or mixed into an enum declaration
+
+// Enum Values
+
+//   - accessible individually by the static name given or by the static values constant
+//     which returns an array of all the declared instances
+
+// Enum Value Index
+
+//   - every value in an enum has an associated index
+
+//   - the index positioning starts off at 0 (zero-based index)
+
+//   - the index assigned to an enums value is in the order in which they
+//     were declared when the enum was declared
+
+// Enum name
+
+//   - by default all fixed values declared within an enum have an associated name property
+//     that returns a string representation of the fixed values name
 
 // When to Use an Enum?
 
