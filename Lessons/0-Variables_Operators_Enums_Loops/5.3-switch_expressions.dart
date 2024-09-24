3d22db3b15c6f10a623047c6840205daf005f2df
Kwe <Kweayon@gmail.com>
2024-09-23 20:49:14 -0500

    refactored lesson

--- Lessons/0-Variables_Operators_Enums_Loops/5.3-switch_expressions.dart
+++ Lessons/0-Variables_Operators_Enums_Loops/5.3-switch_expressions.dart
@@ -5,63 +5,92 @@ typedef VonNeumannModel = ({
   String storage,
 });
 
-final VonNeumannModel macRecord = (
+enum OperatingSystemType {
+  mac,
+  windows,
+  linux;
+}
+
+void main() {
+  const operatingSystems = OperatingSystemType.values;
+
+  final computer0 = switch (operatingSystems[0]) {
+    OperatingSystemType.mac => macRecord,
+    OperatingSystemType.windows => windowsRecord,
+    OperatingSystemType.linux => linuxRecord,
+  };
+
+  print('computer0: $computer0');
+
+  print('computer1: ${switch (operatingSystems[1]) {
+    OperatingSystemType.mac => macRecord,
+    OperatingSystemType.windows => windowsRecord,
+    OperatingSystemType.linux => linuxRecord,
+  }}');
+
+  final computer2 = _computer(operatingSystems[2]);
+
+  print('computer2: $computer2');
+}
+
+const VonNeumannModel macRecord = (
   os: 'macOS Ventura 13.0.1',
   cpu: 'Apple M1',
   memory: '8gb',
   storage: 'Macintoch HD 250gb',
 );
-final VonNeumannModel linuxRecord = (
+
+const VonNeumannModel linuxRecord = (
   os: 'Ubuntu',
   cpu: 'Intel Pentium Gold Processor 8505',
   memory: '16gb',
   storage: 'Seagate Exos X20 20tb',
 );
-final VonNeumannModel windowsRecord = (
+
+const VonNeumannModel windowsRecord = (
   os: 'Windows 11',
   cpu: 'Intel Atom x6200FE',
   memory: '32gb',
   storage: 'Seagate BarraCuda 8TB HDD',
 );
 
-VonNeumannModel getComputer(String os) => switch (os) {
-      'mac' => macRecord,
-      'windows' => windowsRecord,
-      'linux' => linuxRecord,
-      _ => throw FormatException('$os is not an operating system')
+VonNeumannModel _computer(OperatingSystemType os) => switch (os) {
+      OperatingSystemType.mac => macRecord,
+      OperatingSystemType.windows => windowsRecord,
+      OperatingSystemType.linux => linuxRecord,
     };
 
-void main() {
-  final List<String> os = [
-    'mac',
-    'windows',
-    'linux',
-  ];
+// Switch Expression
 
-// Switch statement expression
+//   - produces a value based on the expression body of whichever case matches
 
-//   - return a value from a switch statement:
-//       - assign it to a variable
-//       - use as areturn value of a function
+//   - can be used anywhere expressions are allowed:
 
-  final computer0 = switch (os[0]) {
-    'mac' => macRecord,
-    'windows' => windowsRecord,
-    'linux' => linuxRecord,
-    _ => 'not a known operating system',
-  };
+//       - assign returned value to a variable
 
-  final VonNeumannModel computer1 = switch (os[1]) {
-    'mac' => macRecord,
-    'windows' => windowsRecord,
-    'linux' => linuxRecord,
-    _ => throw FormatException(),
-  };
+//       - used as a return value of a function
 
-  print('computer0: $computer0');
-  print('computer1: $computer1');
+//       - used as an argument to a parameter of a function or Object
 
-  final computer2 = getComputer(os[2]);
+// Switch Expression Syntax
 
-  print('computer2: $computer2');
-}
+//   - Switch expression cases do not start with the case keyword
+
+//   - the structure of a case is similar to an anonymous arrow function and uses arrow function notation
+//     implying case bodies are single expressions instead of a series of statements (no block bodys allowed)
+
+//   - the head of the function is the case pattern to match against and the body is the single expression to evaluate
+
+//       ~ e.g. patternToMatch => expression_to_evaluate, 
+
+//   - there is no fallthrough so each case must have a body (can use Logical OR so this is not much of an issue)
+
+//   - cases are separated by a comma (,) with an optional trailing comma for the final case
+
+// Switch Expression Default / Wildcard _ Clause
+
+//   - default cases can only use _ and not the default keyword
+
+//       ~ e.g. _ => Thow SomeError(),
+
+
