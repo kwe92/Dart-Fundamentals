72d333e8dbda2b040dc8a07198c58dc8147d7a78
Kwe <Kweayon@gmail.com>
2024-09-25 08:46:38 -0500

    refact: updated lesson and comments

--- Lessons/0-Variables_Operators_Enums_Loops/7.0-for_loop.dart
+++ Lessons/0-Variables_Operators_Enums_Loops/7.0-for_loop.dart
@@ -1,28 +1,49 @@
-// For Loops
+int factorial(int x) {
+  var result = 1;
 
-//   - finite loop
-//   - execute code block x number of times
-
-// consists of three parts:
-
-//   - initalizer (instantiated variable)
-//   - condition
-//   - final expression
-
-// Example 1: for loop
-
-int getFactorial(int x) {
-  var factorial = 1;
   for (var i = x; i >= 1; i--) {
-    factorial *= i;
+    result *= i;
   }
-  return factorial;
+  return result;
+}
+
+int factorialRecursive(int x) {
+  if (x <= 1) {
+    return 1;
+  }
+
+  return x * factorialRecursive(x - 1);
 }
 
 void main() {
   final num = 4;
 
-  print(
-    'The factorial of $num is ${getFactorial(num)}',
-  );
+  print('The factorial of $num is ${factorial(num)}');
+
+  print('The factorial of $num is ${factorialRecursive(0)}');
 }
+
+// Control Flow
+
+//   - as the name implies we want to control the flow of our
+//     program given certain conditions or lack thereof
+
+//   - loops and supporting statements are one tool to
+//     control the flow of a program along with branches and exception handling
+
+// Standard for loops
+
+//   - control the flow of a program by iterating through a finite loop
+//      executing a block of code n number of times
+
+//   - for loops have 3 required components
+
+//   - most recursive functions can be handled with some form of a loop
+
+// for loop Components:
+
+//   - initalizer variables / loop variables (at least one is required but may contain more)
+
+//   - condition (the condition that will terminate the loop if false)
+
+//   - final expression / iteration expression (updates the initalizer variables, typically with an augment assignment operator such as ++ or -- to increment and decrement respectively)
