b253d35dbdad58081ae335ab7062767b2892d6be
Kwe <Kweayon@gmail.com>
2024-09-26 11:37:59 -0500

    refact: updated lesson and comments

--- Lessons/1-Collections/1.2-array-enumeration.dart
+++ Lessons/1-Collections/1.2-array-enumeration.dart
@@ -1,3 +1,25 @@
+void main() {
+  final animeCharactersArray = ['Gaara Sabaku', 'Gon Freecss', 'Shikimaru Nara', 'Hotaro Oreki'];
+
+  final arrayToMap = animeCharactersArray.asMap();
+
+  print('\nTransformed List<String> to Map<int, String>: \n\n$arrayToMap\n');
+
+  print('Enumerating over array: ');
+  animeCharactersArray.asMap().forEach((index, value) => print('\nindex: $index: value: $value'));
+  print('\n');
+}
+
+
+  // Enumeration
+
+  //   - List.asMap can also act as a way to enumerate Lists
+
+  //   - use the key from the callback as the index
+
+  //   - .asMap provides a 1-to-1 mapping to the array index
+
+
 // Enumerating Over Arrays
 
 //   - accessing the index of an array
@@ -5,31 +27,9 @@
 //  List.asMap() Method
 
 //    - transforms the List into an unmodifed Map
+
 //        - keys are integers
+
 //        - values are any type you choose
 
 //    - 1-to-1 mapping to array indices
-
-void main() {
-  final List<String> animeCharactersArray = ['Gaara Sabaku', 'Gon Freecss', 'Shikimaru Nara', 'Hotaro Oreki'];
-
-  final Map<int, String> arrayToMap = animeCharactersArray.asMap();
-
-  print('\nTransformed List<String> to Map<int, String>: \n\n$arrayToMap\n');
-
-  // Enumeration
-
-  //   - List.asMap can also act as a way to enumerate Lists
-  //   - use the key from the callback as the index
-  //   - .asMap provides a 1-to-1 mapping to the array index
-
-  print('Enumerating over array: ');
-  animeCharactersArray.asMap().forEach(
-        (
-          int index,
-          String value,
-        ) =>
-            print('\nindex: $index: value: $value'),
-      );
-  print('\n');
-}
