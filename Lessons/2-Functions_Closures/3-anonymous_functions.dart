86f420bc5b5d4e826ca259000f65c7e02b7e2637
Kwe <Kweayon@gmail.com>
2024-10-02 12:30:43 -0500

    refact: updated lesson and comments

--- Lessons/2-Functions_Closures/3-anonymous_functions.dart
+++ Lessons/2-Functions_Closures/3-anonymous_functions.dart
@@ -1,29 +1,38 @@
-// Anonymous Functions:
-
-//   - originate from lambda calculus theory
-//   - unnamed function
-//   - usable where functions are usable
-
-//   - Anonymous Function Names:
-
-//       - closure (functions that retain their lexical scope)
-
-//       - callback:
-
-//           - references to functions in memory not defined in current module
-//           - can be access in other parts of a program
-
 typedef IntFunc = int Function(int);
 void main() {
   // lambda function with body
   () {
     print('basic anonymous function.');
-  };
+  }();
 
   // lambda function as an arrow function
   () => print('basic lambda arrow function.');
 
   int multiply(int x, IntFunc timesFunc) => timesFunc(x);
 
-  print('High Order Function - multiply(3, (p0) => p0 * 4): ${multiply(3, (p0) => p0 * 4)}');
+  print('High Order Function - multiply(3, (n) => n * 4): ${multiply(3, (n) => n * 4)}');
 }
+
+
+// Anonymous Functions:
+
+//   - originate from lambda calculus theory
+
+//   - unnamed function
+
+//   - usable where functions are usable
+
+//   - Anonymous Function Names:
+
+//      - closure:
+
+//          - functions (anonymous or named) that remember and retain access to variables 
+//            from their surrounding environment (lexical scope) even after the outer function has finished executing.
+
+//       - callback:
+
+//           - a first class function that a client (code calling a module) passes to a module
+//             as an argument to be called within the modules implementation
+
+
+//           - can also be aeference to functions in memory
