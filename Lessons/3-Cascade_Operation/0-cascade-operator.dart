d6b35f735ef0d9cbe3fd7a0893fd1a2c122b395b
Kwe <Kweayon@gmail.com>
2024-10-07 22:34:34 -0500

    feat: updated lesson and comments

--- Lessons/3-Cascade_Operation/0-cascade-operator.dart
+++ Lessons/3-Cascade_Operation/0-cascade-operator.dart
@@ -1,43 +1,77 @@
-// Cascade notation
+import 'dart:math';
 
-//   - (.., ?..)
-//   - sequence of operations on the same object
-//   - allowing access to member variables and methods
+enum Colors {
+  black,
+  green,
+  blue,
+  purple,
+}
 
-// why is Cascading useful?
-
-//   - create less temporary variables
-
-// Stateful objects
-
-//   - only seems useful on mutable objects
+enum StrokeCap {
+  butt,
+  round,
+  square,
+}
 
 class Paint {
-  double StrokeWidth;
-  double StrokeCape;
-  String color;
+  Colors? color;
+  StrokeCap? strokeCap;
+  double? strokeWidth;
 
   Paint({
-    required this.StrokeWidth,
-    required this.StrokeCape,
-    required this.color,
+    this.color,
+    this.strokeCap,
+    this.strokeWidth,
   });
+
+  @override
+  String toString() => 'Paint(color: $color, strokeCap: $strokeCap, strokeWidth: $strokeWidth)';
 }
 
 void main() {
-  // instantiate mutable instance
+  final randomInt = Random().nextInt;
 
-  Paint paint = Paint(
-    StrokeWidth: 10.0,
-    StrokeCape: 16.0,
-    color: 'purple',
-  );
+  int randIntRange(int min, int max) => randomInt.call((max - min)) + min;
+
+  // instantiate mutable instance
+  Paint paint = Paint();
+
+  // Reasign Object Properties Using Cascade Notation
+
+  //   - requires only one temporary variable and semicolon for all reassigned properties
+  paint
+    ..color = Colors.values[randomInt.call(Colors.values.length)]
+    ..strokeCap = StrokeCap.values[randomInt.call(StrokeCap.values.length)]
+    ..strokeWidth = randIntRange(1, 17).toDouble();
 
   print(paint);
 
-  // use casecading to reassign
-  paint
-    ..color = 'green'
-    ..StrokeCape = 12.0
-    ..StrokeWidth = 16.0;
+  // Reasign Object Properties Using Default Syntax
+
+  //   - requires a temporary variable and semicolon for all reassigned properties
+
+  paint.color = Colors.values[randomInt.call(Colors.values.length)]; // temporary variable with semicolon
+
+  paint.strokeCap = StrokeCap.values[randomInt.call(StrokeCap.values.length)]; // temporary variable with semicolon
+
+  paint.strokeWidth = randIntRange(1, 17).toDouble(); // temporary variable with semicolon
+
+  print(paint);
 }
+
+// Cascade notation
+
+//   - (.., ?..)
+
+//   - do a sequence of operations on the same object
+//     allowing access to member variables and methods
+
+// why Use Cascade Notation?
+
+//   - reasign properties without the need to specify the object multiple times
+
+//   - create less temporary variables
+
+// Stateful (Mutable) Objects
+
+//   - only seems useful on mutable object
