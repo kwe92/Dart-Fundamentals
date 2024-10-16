19e11490a2ea3f38cdddb60f38a18b1bad1500a3
Kwe <Kweayon@gmail.com>
2024-10-16 15:27:24 -0500

    reaft: updated lesson

--- Lessons/6-OOP/13-generics01.dart
+++ Lessons/6-OOP/13-generics01.dart
@@ -1,35 +1,33 @@
 abstract class Vector<T extends num> {
-  final T i;
-  final T j;
-  final T k;
+  T i;
+  T j;
+  T k;
 
-  const Vector({required this.i, required this.j, required this.k});
+  Vector({required this.i, required this.j, required this.k});
 
-  Vector<T> scale(T scaler);
-
-  Vector<T> add(Vector<T> vector);
+  void scale(T scaler);
 }
 
 class VectorImp<T extends num> extends Vector<T> {
   VectorImp({required super.i, required super.j, required super.k});
 
   @override
-  Vector<T> scale(T scaler) => VectorImp<T>(i: i + scaler as T, j: j + scaler as T, k: k + scaler as T);
-
-  @override
-  Vector<T> add(Vector<num> vector) {
-    // TODO: implement add
-    throw UnimplementedError();
+  void scale(T scaler) {
+    i = i + scaler as T;
+    j = j + scaler as T;
+    k = k + scaler as T;
   }
 
   @override
   String toString() => "Vector(i:$i, j:$j, k:$k)";
 }
 
-void main(List<String> args) {
-  final v0 = VectorImp<double>(i: 1, j: 2, k: 3);
+void main() {
+  final vector = VectorImp<double>(i: 1, j: 2, k: 3);
 
-  final v1 = v0.scale(5.4);
+  print(vector);
 
-  print(v1);
+  vector.scale(5.4);
+
+  print(vector);
 }
