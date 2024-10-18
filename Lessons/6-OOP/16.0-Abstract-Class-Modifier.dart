f50c2f5afbe8dd07aacbfa9b85a4981abd208f47
Kwe <Kweayon@gmail.com>
2024-10-18 11:50:41 -0500

    refact: updated lesson and comments

--- Lessons/6-OOP/16.0-Abstract-Class-Modifier.dart
+++ Lessons/6-OOP/16.0-Abstract-Class-Modifier.dart
@@ -1,101 +1,65 @@
-// Dart 3 Class Modifiers
-
-// Class Modifiers
-
-//   - Prefix class declaration
-
-//   - Adds additional features and restrictions to class declarations
-
-// abstract class modifier
-
-//  - Can not be instantiated (constructed) in any module (library)
-
-//  - function signature implementation is not enforced on sub-types
-
-// abstract methods
-
-//   - replace method body with semi-colon, indicating to the compiler that the method is abstract
-
-//   - Only abstract classes can have abstract methods
-
-// Stateful abstract class Employee
-
 abstract class Employee {
   final String fname;
+
   final String lname;
-  late double _salary;
+
+  double _salary;
 
   double get salary => _salary;
 
   String get email => '${fname.substring(0, 1) + lname}@ayonInnovations.net'.toLowerCase();
 
+  // Generative Constructor with Initializer List
   Employee({
     required this.fname,
     required this.lname,
     required double salary,
-  }) {
-    this._salary = salary;
-  }
+  }) : _salary = salary;
 
   // an abstract method signature that must be implemented by inheriting sub-types
   void giveRaise(double x);
 }
 
 class SoftwareEngineer extends Employee {
-  final List<String> progLangs;
+  final List<String> programmingLanguages;
+
   final List<String> sdks;
+
   SoftwareEngineer({
+    required this.programmingLanguages,
+    required this.sdks,
     required super.fname,
     required super.lname,
-    required this.progLangs,
-    required this.sdks,
     required super.salary,
   });
 
   @override
   void giveRaise(double x) {
-    x <= 0.15
-        ? this._salary = this._salary + (this._salary * x)
-        : print(
-            'raise can not exceed 15%',
-          );
+    x <= 0.15 ? this._salary = this._salary + (this._salary * x) : throw RaiseSalaryError();
   }
 }
 
-class RaiseSalaryError extends Error {
-  final String? message;
-  RaiseSalaryError({this.message = 'Raise amount can not exceed 15%.'});
-}
-
-int main() {
+void main() {
   final emp1 = SoftwareEngineer(
     fname: 'Kweayon',
     lname: 'Clark',
     salary: 130000,
-    progLangs: [
-      'SQL',
-      'Python',
-      'HTML',
-      'CSS',
-      'Dart',
-      'JavaScript',
-      'TypeScript',
-    ],
-    sdks: <String>[
-      'Flutter',
-      'React',
-    ],
+    programmingLanguages: ['SQL', 'Python', 'HTML', 'CSS', 'Dart', 'JavaScript', 'TypeScript', 'Go'],
+    sdks: ['Flutter', 'React'],
   );
 
   print(emp1.email + '\n');
+
   print('Salary: ${emp1.salary}\n');
+
   emp1.giveRaise(0.14);
 
   print('Salary after raise: ${emp1.salary}\n');
+
   try {
     emp1.giveRaise(0.16);
   } catch (error, stackTrace) {
-    print('\n Error Message: $error\n');
+    print('ERROR: ${error.toString()}\n');
     print('Stack Trace: $stackTrace\n');
   } finally {
     print('''
@@ -107,8 +71,63 @@ Again begin this and thou wilt have finished.
 
 ''');
   }
-// no setter therefore an error will occur
-  // emp1.salary = 99999999;
 
-  return 0;
+  // no setter therefore an error will occur
+
+  // emp1.salary = 99999999;
 }
+
+class RaiseSalaryError extends Error {
+  final String message;
+
+  RaiseSalaryError({this.message = 'Raise amount can not exceed 15%.'});
+
+  @override
+  String toString() => message;
+}
+
+// Dart 3 Class Modifiers
+
+// Class Modifiers
+
+//   - Prefixes class declaration
+
+//   - Adds additional features and restrictions to class declarations
+
+// Interface
+
+//   - the highest level of abstraction for an object
+
+//   - defines a contract specifying the set of methods a class must implement
+
+//   - contains only the signatures of methods omitting any implementation details
+
+// abstract class modifier
+
+//   - Some Object-Oriented languages do not have interfaces but rather an Abstract Class that can act as an Interface
+
+//   - abstract classes can declare instance fields and have a set of method signatures to be implemented by sub-types
+
+//   - abstract classes can not be instantiated (constructed) directly in any module (library)
+
+//   - unlike traditional Interfaces Abstract Classes can provide implementations
+//     for methods if they choose too but typically don't
+
+// abstract methods
+
+//   - equivalent to Interface methods, providing only the signature of a method that must be implemented
+//     by sub-types
+
+//   - replace method body with semi-colon, indicating to the compiler that the method is abstract
+
+//   -  abstract methods can only be defined in abstract classes
+
+// Initializing Private Fields as Initializing Formal Parameters
+
+//   - private fields can't be used as named initializing formals
+//     and must be initialized with an initializer list instead
+//     or marked as late and initialized in the constructor body (not idomatic nor recommended)
+
+//   - private fields can be used as positional initializing formals
+
+
