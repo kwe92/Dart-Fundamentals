07a3587e02afabe6b9c3369aea7d49d7d585db3d
Kwe <Kweayon@gmail.com>
2024-10-18 13:08:26 -0500

    refact: updated lesson and comments

--- Lessons/6-OOP/11-null_awareness_deepCopy.dart
+++ Lessons/6-OOP/11-null_awareness_deepCopy.dart
@@ -1,69 +1,46 @@
-// Custom Deep Copy Method: Maintaining Immutablility
-
-//   - Implement a deepCopy method on a class
-//     maintaining immutability of its instances (instantiated objects)
-//   - Copy an object and all of its properties
-//     without referencing the same address in memory
-//   - deepCopy should return a new object of that type with
-//     only the specified properties changed
-
-// Null operator ?? (Null aware operators)
-
-//   - part of the Set of null aware operators
-//   - returns the expression (variable) to the left if
-//     the expression is not null otherwise
-//     returns the expression to the right
-
 class UserCredentials {
-  final String userName;
-  final String userEmail;
-  final String userPassword;
+  final String name;
+  final String email;
+  final String password;
   final bool isAdmin;
 
   const UserCredentials({
-    required this.userName,
-    required this.userPassword,
-    required this.userEmail,
+    required this.name,
+    required this.email,
+    required this.password,
     required this.isAdmin,
   });
 
   UserCredentials copyWith({
-    String? userName,
-    String? userPassword,
-    String? userEmail,
+    String? name,
+    String? password,
+    String? email,
     bool? isAdmin,
   }) =>
       UserCredentials(
-        userName: userName ?? this.userName,
-        userPassword: userPassword ?? this.userPassword,
-        userEmail: userEmail ?? this.userEmail,
+        name: name ?? this.name,
+        email: email ?? this.email,
+        password: password ?? this.password,
         isAdmin: isAdmin ?? this.isAdmin,
       );
 
   @override
-  String toString() => """
-UserCredentials(
-  userName: ${this.userName},
-  userPassword: ${this.userPassword},
-  userEmail: ${this.userEmail},
-  isAdmin: ${this.isAdmin},
-  )
-""";
+  String toString() => 'UserCredentials(name: $name, email: $email, password: $password, isAdmin: $isAdmin)';
 }
 
 void main() {
   final user = UserCredentials(
-    userName: 'Gaara',
-    userPassword: 'SandKing',
-    userEmail: 'kingofsand@naruto.anime',
+    name: 'Gaara',
+    email: 'kingofsand@naruto.anime',
+    password: 'SandKing',
     isAdmin: true,
   );
 
   final UserCredentials updatedUser = user.copyWith(
-    userPassword: '1TailedSandDemonKing',
+    password: '1TailedSandDemonKing',
   );
 
-  final UserCredentials anotherUpdate = updatedUser.copyWith(userEmail: 'HiddenInTheSand@naruto.anime');
+  final UserCredentials anotherUpdate = updatedUser.copyWith(email: 'HiddenInTheSand@naruto.anime');
 
   print('\nCurrent user credentials:\n\n$user\n');
 
@@ -71,3 +48,21 @@ void main() {
 
   print('\nSecond update user credentials:\n\n$anotherUpdate\n');
 }
+// Custom Deep Copy Method: Maintaining Immutablility
+
+//   - Implement a deepCopy method on a class
+//     maintaining immutability of its instances (instantiated objects)
+
+//   - Copy an object and all of its properties
+//     without referencing the same address in memory
+
+//   - deepCopy should return a new object of that type with
+//     only the specified properties changed
+
+// Null operator ?? (Null aware operators)
+
+//   - part of the Set of null aware operators
+
+//   - returns the expression (variable) to the left if
+//     the expression is not null otherwise
+//     returns the expression to the right
