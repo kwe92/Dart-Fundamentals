4f7a4a6cd8c5e8f82861871fe8af8c39af4975f2
Kwe <Kweayon@gmail.com>
2024-10-20 16:16:23 -0500

    refact: updated lesson and comments

--- Lessons/6-OOP/getters_setters.dart
+++ Lessons/6-OOP/getters_setters.dart
@@ -1,26 +1,9 @@
-// getter (Derivation / Computed)
-
-//   - special method providing read access to:
-//       - member variables and computed variables
-
-//   - generated implicitly for all public member vairables
-
-// setter
-
-//  - special method, provides write access to mutable member variables
-
-//  - nullable and late instance variables that have not been initalized implicitly have setter methods
-
-// Nullable mutable Class
-
-//   - constructor can be ommited
-
 class Employee {
   //implicitly has a getter and setter method
   String? fname, lname;
   double? salary;
 
-  // derivation
+  // Getter derivation evaluating to a derivative field
   String? get email => '$fname.$lname@ayonInovations.net';
 
   // print string representation of Object
@@ -29,8 +12,8 @@ class Employee {
 }
 
 void main() {
-  // implicitly initalize all member variables as Null
-  Employee emp1 = Employee();
+  // implicitly initalize all member variables as null
+  var emp1 = Employee();
 
   print('emp1: $emp1');
 
@@ -42,3 +25,38 @@ void main() {
 
   print('emp1 after setting variables: $emp1');
 }
+
+// getter Methods (Derivation / Computed Field)
+
+//   - special methods providing read access to:
+
+//       - instance variables and computed variables (derived fields)
+
+//   - generated implicitly for all instance vairables
+
+//   - can be implemented explicitly to generate derived instance variables (computed variables) 
+
+//   - computed variables can be derived from:
+
+//       - other instance variables within the same class
+
+//       - other parameters from another object (instance of a class)
+//         such as a service object
+
+//       - a function call
+
+//   - getter methods are evaluated lazily
+
+// setter Methods
+
+//  - special methods providing write access to mutable member variables
+
+//  - nullable and late instance variables that have not been initalized implicitly have setter methods
+
+// Uniform Access Principle
+
+//   - whether a property (field of a class) is computed or stored is an implementation detail
+
+//   - getters are implemented implcitly and setters are implemented depending
+//     on the constraints of the variable when it was declared
+//     i.e. is the instance variable static, non-nullable, nullable, late, final, or const
