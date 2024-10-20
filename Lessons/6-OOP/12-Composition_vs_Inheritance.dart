d0450d23b950a6f960d891b2bbaafef3b44f3848
Kwe <Kweayon@gmail.com>
2024-10-19 21:54:09 -0500

    refact: updated lesson and comments

--- Lessons/6-OOP/12-Composition_vs_Inheritance.dart
+++ Lessons/6-OOP/12-Composition_vs_Inheritance.dart
@@ -1,34 +1,24 @@
-// Composition over Inheritance
-
-//   - inheritance is the process of a sub-class
-//     inheriting variables and methods of a super-class
-//     inheritance tightly couples the sub-class to the super-class implementation
-//     the sub-classes ability to re-implement the methods
-//     of a super-class in a diffrent way can lead to some difficult U.I. bugs
-//     in many modern frameworks
-
-// Composition
-
-//   - composition decouples implementations
-//     by separating classes entirely
-//   - a User Interface is made up of small components or widgets
-//   - that act as constituent parts of a whole User Interfaces
-//   - allowing separation of concerns
-//   - keeps code D.R.Y
-//   - encapsulates isolated parts of your User Interface
-//     into individual instantiated objects
-
 // Simulating a U.I.
 
-// Abstract Widget Class to be inherited
-abstract class Widget extends Object {
-  const Widget();
+abstract class Key<T extends Object> {
+  final String value;
+
+  Key(this.value);
+
+  const Key.empty() : value = '';
 }
 
-// Text widget which inherits Widget
+// Abstract Widget Class to be inherited (extended)
+abstract class Widget extends Object {
+  final Key? key;
+
+  const Widget({this.key});
+}
+
+// Text Widget which inherits (extends) Widget
 class Text extends Widget {
   final String text;
-  const Text(this.text);
+  const Text(this.text, {super.key});
 
   @override
   String toString() => '$text';
@@ -36,7 +26,7 @@ class Text extends Widget {
 
 typedef VoidCallback = void Function();
 
-// Button Widget
+// Button Widget which inherits (extends) Widget
 class Button extends Widget {
   final Widget child;
   final VoidCallback onPressed;
@@ -44,19 +34,56 @@ class Button extends Widget {
   const Button({
     required this.child,
     required this.onPressed,
-  });
+    Key? key,
+  }) : super(key: key);
 }
-
-final VoidCallback handleOnPressed = () => {
-      print('Button pressed'),
-    };
 
 void main() {
-  final Button buttonComponent = Button(
+  // Composition of widgets
+  final buttonComponent = Button(
     child: Text('I am a Text Widget'),
-    onPressed: handleOnPressed,
+    onPressed: () {
+      print('Button pressed');
+    },
   );
 
-  print('\n child propery of button: ${buttonComponent.child}\n');
-  print('\n onPressed propery of button: ${buttonComponent.onPressed}\n');
+  print('\n' + 'child propery of button: ${buttonComponent.child}\n');
+  print('åonPressed propery of button: ${buttonComponent.onPressed}\n');
 }
+
+
+// Composition over Inheritance
+
+//   - build complex objects by combining simpler objects
+
+// Inheritance
+
+//   - Inheritance, a mechanism in object-oriented programming where a subclass inherits variables (attributes / fields)
+//     and methods (functions) from a superclass, creating an "is-a" relationship (a subclass "is a" type of the superclass)
+
+//   - promotes code reuse by avoiding duplication of common functionality
+
+//   - enables the extension of existing behavior, making it possible to create new, specialized objects that build upon existing ones
+
+//   - Inheritance creates a tight coupling between subclasses and superclasses, as changes to the superclass can affect all its subclasses
+
+//   - Subclasses can override methods inherited from the superclass, allowing for customization of behavior
+//     however, overriding methods requires careful consideration to ensure compatibility with the superclass's design
+//     and to avoid unintended side effects due to inappropriate subclass implementations.
+
+// Composition
+
+//   - composition is a technique in object-oriented programming that achieves loose coupling between classes (decoupling implementations)
+//     by using instances of other classes as components within a class
+
+//   - complex objects are built by combining instances of simpler objects
+//     creating a "has-a" relationship for example
+//     a User Interface might be composed of individual widgets, each with its own functionality
+
+//   - this approach promotes separation of concerns, making code easier to understand, maintain, and refactor
+
+//   - composition also helps to keep code DRY (Don't Repeat Yourself)
+//     by allowing common functionality to be reused across different objects
+
+//   - by encapsulating individual components into separate objects
+//     composition promotes modularity and reduces the impact of changes to individual parts of the system
