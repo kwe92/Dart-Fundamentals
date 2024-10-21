b6f347cf2cd8c435b041f653e673631454e57a03
Kwe <Kweayon@gmail.com>
2024-10-21 09:49:36 -0500

    refact: updated lesson and comments

--- Lessons/6-OOP/initalizing-formal-parameters-non-null.dart
+++ Lessons/6-OOP/initalizing-formal-parameters-non-null.dart
@@ -1,41 +1,12 @@
-// Initalizing Non-Null Formal Parameters
-
-//   - member variables can be initalized before the constructor body
-//     by passing this.memberVariable into the head of the constructor function
-
-//   - the constructor body can then be ommited for brevity requiring less code
-
-//   - non-nullable member variables must be initalized
-//     in the constructor function head or assigned a default value
-
-// Mutable (Stateful) Class
-
-//   - member variables are mutable if they are
-//     not prefixed with final or const upon declaration
-
-// non-nullable variables
-
-//   - the data type declared for a variable
-//     is not suffixed with [?]
-
-// Mutable Class
 class AnimeCharacter {
-  String fname, lname, anime;
-  AnimeCharacter(this.fname, this.lname, this.anime) {
-    this.fname = fname;
-    this.lname = lname;
-    this.anime = anime;
-  }
-}
+  final String fname, lname, anime;
 
-class AnimeCharacter2 {
-  String fname, lname, anime;
-  AnimeCharacter2(this.fname, this.lname, this.anime);
-  // constructor body is ommited
+  // initalizing positional formal parameters in the head of the Generative Constructor
+  AnimeCharacter(this.fname, this.lname, this.anime);
 }
 
 void main() {
-  final AnimeCharacter2 animeCharacter2 = AnimeCharacter2(
+  final animeCharacter2 = AnimeCharacter(
     'Gon',
     'Freecss',
     'Hunter x Hunter',
@@ -43,3 +14,19 @@ void main() {
 
   print('\nAnother one of my favorite animes: ${animeCharacter2.anime}\n');
 }
+
+
+// Initalizing Formal Parameters
+
+//   - member variables can be initalized before the constructor body
+//     by passing this.memberVariable into the head of the constructor function
+
+//   - the constructor body can then be ommited for brevity requiring less code
+
+//   - final member variables must be initalized
+//     in the constructor function head or assigned a default value
+
+// non-nullable variables
+
+//   - the data type declared for a variable
+//     is not suffixed with [?]
--- Lessons/6-OOP/initalizing-formal-parameters-null.dart
+++ Lessons/6-OOP/initalizing-formal-parameters-null.dart
@@ -1,33 +1,10 @@
-// Initalizing Null Formal Parameters
-
-// Declaring Null member variables
-
-//   - member variables declared null get initalized as Null Objects implicitly
-
-//   - to declare a null member variable suffix its type with [?]
-//       - e.g.
-//           - String? would declare a member
-//             variable as a mutable nullable String
-
-// Class with nullable member variables
-
-//   - constructor can be ommited entirely
-
-//   - if the constructor is ommited there is an
-//     unnamed no arguement constructor generated
-
-//   - no arguement constructor implies you must instantiate the class
-//     without arguments or use cascading during instantiation
-
-// Mutable (Stateful) Nullable Class
-//   the constructor can be ommited entirely
 class AnimeCharacter3 {
   String? fname, lname, anime;
 }
 
 void main() {
   // creating an Object
-  final AnimeCharacter3 animeCharacter3 = AnimeCharacter3();
+  final animeCharacter3 = AnimeCharacter3();
 
   // assign values
   animeCharacter3
@@ -36,11 +13,30 @@ void main() {
     ..anime = 'Naruto';
 
   // instantiate && assign values simultaneously
-  final AnimeCharacter3 animeCharacter4 = AnimeCharacter3()
+  final animeCharacter4 = AnimeCharacter3()
     ..fname = 'Houtarou'
     ..lname = 'Oreki'
     ..anime = 'Hyouka';
 
-  print('\nfavorite anime character: ${animeCharacter3.fname} ${animeCharacter3.lname}\n');
-  print('\nAnother favorite anime character: ${animeCharacter4.fname} ${animeCharacter4.lname}\n');
+  print('favorite anime character: ${animeCharacter3.fname} ${animeCharacter3.lname}\n');
+  print('Another favorite anime character: ${animeCharacter4.fname} ${animeCharacter4.lname}\n');
 }
+
+
+// Initalizing Null Formal Parameters
+
+// Declaring Null member variables
+
+//   - instance fields declared null get initalized as null Objects implicitly
+
+//   - to declare a nullable variable suffix its type annotation with [?]
+//       - e.g.
+//           - String? nullable string
+
+
+// Class with all nullable member variables
+
+//   - constructor can be ommited entirely
+
+//   - if the constructor is ommited there is an
+//     unnamed no arguement constructor generated (Default Constructor)
