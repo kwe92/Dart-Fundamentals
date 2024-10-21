44397293353fd677773ce816d238cc7a53144a21
Kwe <Kweayon@gmail.com>
2024-10-21 10:13:11 -0500

    refact: updated lesson and comments

--- Lessons/6-OOP/no-default-super-constructors.dart
+++ Lessons/6-OOP/no-default-super-constructors.dart
@@ -1,17 +1,5 @@
-// Super Class No Default Constructor (No Generative Constructor)
-
-//   - if a super-class does not have a default constructor
-//     then a unnamed no-argument constructor is created
-
-// Super-class Named Constructor
-
-//   - must initalize the super class named constructor in
-//     the sub-class with an Initalizer List
-
-// Mutable Nullable Class
-
 class Person {
-  String? fname, lname;
+  final String fname, lname;
 
   Person.fromJSON(Map<String, dynamic> json)
       : fname = json['fname'],
@@ -24,6 +12,7 @@ class Person {
 class Engineer extends Person {
   final List<String> progLangs;
 
+  // with super-initializer parameters, parameters do not have to be passed to super constructor
   Engineer.fromJSON(super.json)
       : progLangs = json['progLangs'] as List<String>,
         super.fromJSON();
@@ -35,37 +24,21 @@ class Engineer extends Person {
 class DataAnalyst extends Engineer {
   final List<String> analyticsTools;
 
-  DataAnalyst.fromJSON(super.json)
+  // without super-initializer parameters, parameters must be passed to supper constructor
+  DataAnalyst.fromJSON(Map<String, dynamic> json)
       : analyticsTools = json['analyticsTools'],
-        super.fromJSON();
+        super.fromJSON(json);
 
   @override
   String toString() => 'DataAnalyst(fname: $fname, lname: $lname, progLangs: $progLangs, analyticsTools: $analyticsTools)';
 }
 
 void main() {
-  final Map<String, String> personHashMap = {
-    'fname': 'Baki',
-    'lname': 'Hanma',
-  };
+  final person1 = Person.fromJSON(personHashMap);
 
-  final Map<String, dynamic> engineerHashMap = {
-    'fname': 'Baki',
-    'lname': 'Hanma',
-    'progLangs': ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Python', 'SQL', 'Dart']
-  };
+  final engineer1 = Engineer.fromJSON(engineerHashMap);
 
-  final Map<String, dynamic> dataAnalyst = {
-    'fname': 'Baki',
-    'lname': 'Hanma',
-    'progLangs': ['Python', 'SQL'],
-    'analyticsTools': ['Power BI', 'Tableau']
-  };
-  final Person person1 = Person.fromJSON(personHashMap);
-
-  final Engineer engineer1 = Engineer.fromJSON(engineerHashMap);
-
-  final DataAnalyst dataAnalyst1 = DataAnalyst.fromJSON(dataAnalyst);
+  final dataAnalyst1 = DataAnalyst.fromJSON(dataAnalyst);
 
   print('person1: $person1');
 
@@ -73,3 +46,31 @@ void main() {
 
   print('dataAnalyst1: $dataAnalyst1');
 }
+
+final personHashMap = {
+  'fname': 'Baki',
+  'lname': 'Hanma',
+};
+
+final engineerHashMap = {
+  'fname': 'Baki',
+  'lname': 'Hanma',
+  'progLangs': ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Python', 'SQL', 'Dart']
+};
+
+final dataAnalyst = {
+  'fname': 'Baki',
+  'lname': 'Hanma',
+  'progLangs': ['Python', 'SQL'],
+  'analyticsTools': ['Power BI', 'Tableau']
+};
+
+// Super Class No Default Constructor (No Generative Constructor)
+
+//   - if a super-class does not have a default constructor
+//     then a unnamed no-argument constructor is created
+
+// Super-class Named Constructor
+
+//   - must initalize the super class named constructor in
+//     the sub-class with an Initalizer List
