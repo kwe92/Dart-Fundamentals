0561dab7a480524b327541dc8ba357cf6e86c92e
Kwe <Kweayon@gmail.com>
2024-10-21 10:33:12 -0500

    refact: updated lessonand comments

--- Lessons/6-OOP/14-Operator_Overloading.dart
+++ Lessons/6-OOP/14-Operator_Overloading.dart
@@ -1,15 +1,3 @@
-// Operator overloading
-
-//   - a feature found in most OOP languages distinct from method overriding
-//   - operator overloading is a compile-time polymorphism
-//   - user-defined custom implementations
-//     of common operators such as:
-//       - Arithmetic operators: +, -, *
-//       - Equality operators: == and !=
-//       - Relational operators: >, >=, <, <=
-//       - many more
-//   - e.g. the addition of two strings concatenates them
-
 class Person {
   final String fname;
   final String lname;
@@ -38,15 +26,6 @@ class Person {
   String toString() => 'Person(fname: ${fname}, lname: ${lname}, age: ${age})';
 }
 
-class TypeError extends Error {
-  final String? message;
-
-  TypeError([this.message]);
-
-  @override
-  String toString() => message ?? super.toString();
-}
-
 class Family {
   final List<Person> people;
   const Family({required this.people});
@@ -56,10 +35,10 @@ class Family {
       return Family(people: this.people + other.people);
     }
     if (other is Person) {
-      final List<Person> additionalFamily = [...this.people, other];
+      final additionalFamily = [...this.people, other];
       return Family(people: additionalFamily);
     }
-    throw TypeError('\nThe type must be either Person or Family:\n\nRecieved a type of: ${other.runtimeType}');
+    throw TypeError('The type must be either Person or Family:\nRecieved a type of: ${other.runtimeType}');
   }
 
   @override
@@ -67,45 +46,76 @@ class Family {
 }
 
 void main() {
-  final Person animeCharacter0 = Person(
+  final animeCharacter0 = Person(
     fname: 'Hinata',
     lname: 'Hyuga',
     age: 12,
   );
-  final Person animeCharacter1 = Person(
+  final animeCharacter1 = Person(
     fname: 'Neji',
     lname: 'Hyuga',
     age: 14,
   );
 
-  final Person animeCharacter2 = Person(fname: 'Hiashi', lname: 'Hyuga', age: 42);
+  final animeCharacter2 = Person(fname: 'Hiashi', lname: 'Hyuga', age: 42);
 
-  final Person animeCharacter3 = Person(fname: 'Hizashi', lname: 'Hyuga', age: 42);
+  final animeCharacter3 = Person(fname: 'Hizashi', lname: 'Hyuga', age: 42);
 
-  final Person animeCharacter4 = Person(
+  final animeCharacter4 = Person(
     fname: 'Naruto',
     lname: 'Uzumaki',
     age: 13,
   );
 
-  final Family hyugaSiblings = animeCharacter0 + animeCharacter1;
+  final hyugaSiblings = animeCharacter0 + animeCharacter1;
 
-  final Family hyugaElderTwins = animeCharacter2 + animeCharacter3;
+  final hyugaElderTwins = animeCharacter2 + animeCharacter3;
 
-  final Family hyugaClan = hyugaSiblings + hyugaElderTwins;
+  final hyugaClan = hyugaSiblings + hyugaElderTwins;
 
-  final Family uzumakiClan = Family(people: [animeCharacter4]);
+  final uzumakiClan = Family(people: [animeCharacter4]);
 
-  final Family uzumakiClanExtended = uzumakiClan + animeCharacter0.copyWith(lname: 'Uzumaki');
+  final uzumakiClanExtended = uzumakiClan + animeCharacter0.copyWith(lname: 'Uzumaki');
 
-  print('\nHyuga Clan: $hyugaClan\n');
-  print('\nUzumakiClan Clan: $uzumakiClan\n');
-  print('\nHinata joins the Uzumaki Clan: $uzumakiClanExtended\n');
+  print('Hyuga Clan: $hyugaClan\n');
+  print('UzumakiClan Clan: $uzumakiClan\n');
+  print('Hinata joins the Uzumaki Clan: $uzumakiClanExtended\n');
 
   try {
     uzumakiClanExtended + 'I should throw an error';
   } catch (error, st) {
     print('$error\n');
-    print('Stack Trace:\n\n$st');
-  } finally {}
+    print('Stack Trace:n$st');
+  }
 }
+
+class TypeError extends Error {
+  final String? message;
+
+  TypeError([this.message]);
+
+  @override
+  String toString() => message ?? super.toString();
+}
+
+
+// Operator overloading
+
+//   - add or override the implementation of operators
+
+//   - a feature found in most OOP languages distinct from method overriding
+
+//   - operator overloading is a compile-time polymorphism
+
+//   - user-defined custom implementations
+//     of common operators such as:
+
+//       - Arithmetic operators: +, -, *
+
+//       - Equality operators: == and !=
+
+//       - Relational operators: >, >=, <, <=
+
+//       - many more
+
+//   - e.g. the addition of two strings concatenates them
