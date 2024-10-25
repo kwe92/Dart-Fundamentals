d36bcfcf398ada957de2d5a2eaec2540429e6e0a
Kwe <Kweayon@gmail.com>
2024-10-25 17:12:58 -0500

    refact: edit lesson and comments

---
+++ Lessons/15-asynchronousProgramming/animeMemeService/services/ani_memes_service.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'dart:io';
+import '../../workingWithLocalEndpoints/services/api_service.dart';
+import '../models/ani_memes_model.dart';
+
+class AniMemeService extends ApiService {
+  @override
+  String get host => "https://vast-puce-mite-fez.cyclic.app";
+
+  @override
+  Map<String, String> get headers => {HttpHeaders.contentTypeHeader: "application/json"};
+
+  Future<AniMemesModel> getMeme() async {
+    final response = await get("/animeme");
+
+    final aniMeme = AniMemesModel.fromJSON(jsonDecode(response.body));
+
+    return aniMeme;
+  }
+}
