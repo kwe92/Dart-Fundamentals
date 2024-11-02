f6435a9900ba8b473ca17a64270a02f20ada1bf6
Kwe <Kweayon@gmail.com>
2024-11-02 13:58:36 -0500

    refact: updated lesson and comments

--- Lessons/15-asynchronousProgramming/0-futures/aggregatingFutures/aggregating_futures.dart
+++ Lessons/15-asynchronousProgramming/0-futures/aggregatingFutures/aggregating_futures.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 Future<void> main() async {
+  // start all asynchronous operations at once and return the result of the first Future to complete
   final String result = await Future.any([
     fetchFromServer1(),
     fetchFromServer2(),
@@ -7,6 +10,8 @@ Future<void> main() async {
 
   print(result);
 
+  // start all asynchronous operations at once and wait for the longest running Future to complete
+  // then return all of their results
   final List<String> result2 = await Future.wait([
     fetchFromServer1(),
     fetchFromServer2(),
@@ -16,28 +21,35 @@ Future<void> main() async {
   print(result2);
 }
 
+// Asynchronous Functions to Simulate Fetching Data From a Server
+
 Future<String> fetchFromServer1() async {
-  print('started fetching data from server 1');
+  print('started fetching data from server 1...');
 
   await Future.delayed(const Duration(seconds: 2));
 
-  return 'Some data from some web server.';
+  return 'Some data from some web server 1.';
 }
 
 Future<String> fetchFromServer2() async {
-  print('started fetching data from server 2');
+  print('started fetching data from server 2...');
 
   await Future.delayed(const Duration(seconds: 3));
 
-  throw Exception('I failed to reach the server.');
+  final randomFailure = Random().nextBool();
+
+  if (randomFailure) {
+    throw Exception('I failed to reach the server.');
+  }
+  return 'Some data from some web server 2.';
 }
 
 Future<String> fetchFromServer3() async {
-  print('started fetching data from server 3');
+  print('started fetching data from server 3...');
 
   await Future.delayed(const Duration(seconds: 10));
 
-  return 'I am taking a long time to get server response.';
+  return 'I took a long time to get a response from server 3.';
 }
 
 // Aggregating Futures
@@ -47,14 +59,15 @@ Future<String> fetchFromServer3() async {
 
 // Future.wait - Logical AND | logically equivalent to TypeScripts Promise.all()
 
-//   - Combines an iterable collection of Futures into a single Future
+//   - combines an iterable collection of Futures into a single Future
 //     that waits for all Futures to be complete or fail
 
-//   - returns an array of their results
+//   - returns an array of their results in the order in which the Futures were called
+//     not in the order in which the Futures complete
 
 //   - if any of the Futures fail then the entire Future.wait() fails
 
-//   - great for executingmultiple concurrent operations at once that do not depend on eachother
+//   - great for executing multiple concurrent operations at once that do not depend on eachother
 //     but all of them need to be completed to proceed
 
 //     -  e.g. the start up logic of an application which may require the completion of
@@ -64,5 +77,5 @@ Future<String> fetchFromServer3() async {
 
 //   - returns the first Future in an iterable of futures that completes
 
-//   - e.g. you send multiple requests to a service with multiple servers and you only care
+//   - e.g. you send multiple requests for a service with multiple servers and you only care
 //     about the fastest one to be returned
