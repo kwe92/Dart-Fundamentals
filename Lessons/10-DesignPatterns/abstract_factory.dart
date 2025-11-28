3533ab00366381b8c4ba426b09f2e55d66b56e8a
Kwe <Kweayon@gmail.com>
2025-11-27 18:22:47 -0600

    abstract factory example

--- Lessons/10-DesignPatterns/abstract_factory.dart
+++ Lessons/10-DesignPatterns/abstract_factory.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 enum Direction {north, south, east, west}
 
 abstract class MapSite{
@@ -7,8 +9,9 @@ abstract class MapSite{
 class Room extends MapSite{
   final int roomNo;
   static Set<int> _currentRoomNums = {};
-  List _sides = List.filled(4, 0);
+  static get currentRoomNums => _currentRoomNums;
 
+  List _sides = List.filled(4, 0);
   List get sides => _sides;
 
   Room({required this.roomNo}){
@@ -41,6 +44,25 @@ class Room extends MapSite{
   }
 }
 
+class Spell {
+  final name;
+  Spell({required this.name});
+
+  @override
+  String toString() => 'Spell(name: $name)';
+}
+
+class EnchantedRoom extends Room{
+  final Spell activeSpell;
+  EnchantedRoom({
+    required super.roomNo,
+    required this.activeSpell
+  });
+
+  @override
+  String toString() => 'Room(roomNo: $roomNo, activeSpell: $activeSpell)';
+}
+
 class Door extends MapSite{
   final Room r1;
   final Room r2;
@@ -55,6 +77,18 @@ class Door extends MapSite{
   void enter(){
     // TODO: implement method
   }
+}
+
+class DoorNeedingSpell extends Door {
+  final Spell requiredSpell;
+  DoorNeedingSpell({
+    required super.r1,
+    required super.r2,
+    required this.requiredSpell
+  });
+
+  @override
+  void enter(){}
 }
 
 class Wall extends MapSite{
@@ -77,21 +111,49 @@ class Maze {
   }
 }
 
+class MazeFactory {
+  Maze makeMaze()=> Maze();
+  Wall makeWall()=> Wall();
+  Room makeRoom({required int roomNo})=> Room(roomNo: roomNo);
+  // void makeRooms({List<int> roomNums}){
+  //   for(int i = 0; i < roomNums.length; i++){
+  //   }
+  // }
+  Door makeDoor({required Room r1, required Room r2})=> Door(r1:r1, r2:r2);
+
+}
+
+class EnchantedMazeFactory extends MazeFactory {
+  final List<Spell> roomSpells;
+  final List<Spell> doorSpells;
+
+  EnchantedMazeFactory({required this.roomSpells, required this.doorSpells});
+
+  @override
+  Room makeRoom({required int roomNo}) => EnchantedRoom(roomNo: roomNo, activeSpell: createSpell(getRandom([for(int i = 0; i < roomSpells.length; i++) roomSpells[i].name])));
+
+  @override
+  Door makeDoor({required Room r1, required Room r2}) => DoorNeedingSpell(r1:r1, r2:r2, requiredSpell: createSpell(getRandom([for(int i = 0; i < doorSpells.length; i++) doorSpells[i].name])));
+
+  Spell createSpell(String name) => Spell(name: name);
+
+}
+
 class MazeGame {
-  static Maze createMaze(){
-    final maze = Maze();
-    final r1 = Room(roomNo: 1);
-    final r2 = Room(roomNo: 2);
-    final door = Door(r1: r1, r2: r2);
-
-    r1.setSide(Direction.north, Wall());
+  static Maze createMaze({required MazeFactory mazeFactory}){
+    final maze = mazeFactory.makeMaze();
+    final r1 = mazeFactory.makeRoom(roomNo: Room.currentRoomNums.length > 0 ? Room.currentRoomNums.last + 1 : 1);
+    final r2 = mazeFactory.makeRoom(roomNo: Room.currentRoomNums.last + 1);
+    final door = mazeFactory.makeDoor(r1: r1, r2: r2);
+    final walls = [for(int i = 0; i < 6; i++) mazeFactory.makeWall()];
+    r1.setSide(Direction.north, walls[0]);
     r1.setSide(Direction.east, door);
-    r1.setSide(Direction.south, Wall());
-    r1.setSide(Direction.west, Wall());
+    r1.setSide(Direction.south, walls[1]);
+    r1.setSide(Direction.west, walls[2]);
 
-    r2.setSide(Direction.north, Wall());
-    r2.setSide(Direction.east, Wall());
-    r2.setSide(Direction.south, Wall());
+    r2.setSide(Direction.north, walls[3]);
+    r2.setSide(Direction.east, walls[4]);
+    r2.setSide(Direction.south, walls[5]);
     r2.setSide(Direction.west, door);
 
     maze.addRoom(r1);
@@ -101,12 +163,38 @@ class MazeGame {
   }
 }
 
-void main(){
-  
-  final maze = MazeGame.createMaze();
-  print(maze.rooms);
+T getRandom<T>(List<T> items){
+  final random = Random();
 
-  for(var room in maze.rooms){
-    print(room.sides);
+  final randomIndex = random.nextInt(items.length);
+
+  // Retrieve the item at the random index
+  T randomItem = items[randomIndex];
+
+  return randomItem;
+}
+
+void main(){
+  final mazeFactory = MazeFactory();
+  final enchantedMazeFactory = EnchantedMazeFactory(roomSpells: [Spell(name:'Enhanced map'), Spell(name:'Intangibility'), Spell(name:'Unlock IV')],
+                                                    doorSpells: [Spell(name:'Lock I'),Spell(name:'Lock III'),Spell(name:'Lock V')]);
+  final maze = MazeGame.createMaze(mazeFactory: mazeFactory);
+  final enchanteMaze = MazeGame.createMaze(mazeFactory: enchantedMazeFactory);
+
+  print(maze.rooms);
+  print(enchanteMaze.rooms);
+
+  // for(var room in maze.rooms){
+  //   print(room.sides);
+  // }
+
+  // for(var room in enchanteMaze.rooms){
+  //   print(room.sides);
+  // }
   }
-  }
+
+  // ABSTRACT FACTORY
+
+  // Intent
+  //  - Provide an interface for creating families of related or dependent objects without
+  //    specifying their concrete classes.
