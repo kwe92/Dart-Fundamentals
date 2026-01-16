b91cbdafe45dc7adadaaf3d041b63c10fa4840b1
Kwe <Kweayon@gmail.com>
2026-01-16 07:41:47 -0600

    updated example and added summary comments

--- Lessons/10-DesignPatterns/behavioral/chainOfResponsibility/chain_of_responsibility.dart
+++ Lessons/10-DesignPatterns/behavioral/chainOfResponsibility/chain_of_responsibility.dart
@@ -1,20 +1,23 @@
-typedef Topic = int;
+// Help Request Topics
 
-const Topic NO_HELP_TOPIC = -1;
-const Topic PRINT_TOPIC = 1;
-const Topic PAPER_ORIENTATION_TOPIC = 2;
-const Topic APPLICATION_TOPIC = 3;
+enum Topic {
+  NO_HELP_TOPIC,
+  PRINT_TOPIC,
+  PAPER_ORIENTATION_TOPIC,
+  APPLICATION_TOPIC;
+}
 
+// Interface for handling help requests
 abstract class HelpHandler {
   HelpHandler? _successor;
   Topic _topic;
 
-  HelpHandler([HelpHandler? handler, Topic topic = NO_HELP_TOPIC])
+  HelpHandler([HelpHandler? handler, Topic topic = Topic.NO_HELP_TOPIC])
       : _successor = handler,
         _topic = topic;
 
   bool hasHelp() {
-    return _topic != NO_HELP_TOPIC;
+    return _topic != Topic.NO_HELP_TOPIC;
   }
 
   void setHandler(HelpHandler handler, Topic topic) {
@@ -25,7 +28,7 @@ abstract class HelpHandler {
   // TEMPLATE METHOD (replace some but not all of an algorithm (i.e. function implementation))
   void handleHelp() {
     if (hasHelp()) {
-      additionalHelp(); // Call the specific polymprhic action
+      additionalHelp(); // Call the specific polymorphic action
     } else if (_successor != null) {
       print('${this} cannot handle the request passing request to successor ${_successor}...');
       _successor!.handleHelp(); // Pass down the chain
@@ -34,14 +37,15 @@ abstract class HelpHandler {
     }
   }
 
-  // Hook method, inteded for Subclasses to override
+  // Hook method, intended for Subclasses to override
   void additionalHelp();
 
   @override
   String toString() {
-    final instanceStr = super.toString().split(' ');
-    final typeStr = instanceStr[instanceStr.length - 1];
-    return typeStr;
+    final typeStr = super.toString().split(' ');
+    final typeNameIndex = typeStr.length - 1;
+    final result = typeStr[typeNameIndex].replaceAll(RegExp(r"'"), '');
+    return result;
   }
 }
 
@@ -49,28 +53,28 @@ abstract class Widget extends HelpHandler {
   // ignore: unused_field
   Widget? _parent;
 
-  Widget(Widget? parent, [Topic t = NO_HELP_TOPIC])
+  Widget(Widget? parent, [Topic t = Topic.NO_HELP_TOPIC])
       : _parent = parent,
         super(parent, t);
 }
 
 class Button extends Widget {
-  Button(Widget? parent, [Topic topic = NO_HELP_TOPIC]) : super(parent, topic);
+  Button(Widget? parent, [Topic topic = Topic.NO_HELP_TOPIC]) : super(parent, topic);
 
   @override
   void additionalHelp() {
-    print('Button can handle request for ${_topic}');
+    print('Button can handle request for ${_topic.name}');
   }
 }
 
 class Dialog extends Widget {
-  Dialog(HelpHandler successor, [Topic topic = NO_HELP_TOPIC]) : super(null) {
+  Dialog(HelpHandler successor, [Topic topic = Topic.NO_HELP_TOPIC]) : super(null) {
     setHandler(successor, topic);
   }
 
   @override
   void additionalHelp() {
-    print('Dialog can handle request for ${_topic}');
+    print('Dialog can handle request for ${_topic.name}');
   }
 }
 
@@ -79,14 +83,40 @@ class Application extends HelpHandler {
 
   @override
   void additionalHelp() {
-    print('Application can handle request for ${_topic}');
+    print('Application can handle request for ${_topic.name}');
   }
 }
 
 void main() {
-  Application application = new Application(APPLICATION_TOPIC);
+  Application application = new Application(Topic.APPLICATION_TOPIC);
   Dialog dialog = new Dialog(application);
   Button button = new Button(dialog);
 
   button.handleHelp();
 }
+
+// 1. Core Identity
+
+      // Pattern Name: 
+      // Category: 
+      // Intent:
+
+// 2. The Problem & Solution (Motivation)
+
+      // Scenario:
+      // Problem: 
+      // Solution:
+
+// 3. Participants
+
+
+// 4. Consequences
+
+
+// 5. Implementation Issues
+
+
+// 6. Related Patterns
+
+
+// 7. Sample Code Use Case & Analysis (Optional)
